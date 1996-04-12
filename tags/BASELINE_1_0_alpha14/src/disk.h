d1c66b8adcdffb50f71b0a982b7026687c4e6bd9
nobody <nobody@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
1996-04-12 00:11:02 +0000

    This commit was manufactured by cvs2svn to create tag 'BASELINE_1_0_alpha14'.

---
+++ tags/BASELINE_1_0_alpha14/src/disk.h
@@ -0,0 +1,43 @@
+/*  $Id$ */
+
+#ifndef DISK_H
+#define DISK_H
+
+#define DISK_OK                   (0)
+#define DISK_ERROR               (-1)
+#define DISK_EOF                 (-2)
+#define DISK_WRT_LOCK_FAIL       (-3)
+#define DISK_WRT_WRONG_CODE      (-4)
+#define DISK_FILE_NOT_FOUND      (-5)
+#define DISK_NO_SPACE_LEFT       (-6)
+
+typedef int (*FILE_READ_HD) _PARAMS((int fd, char *buf, int size, int errflag,
+	void *data, int offset));
+
+typedef int (*FILE_WALK_HD) _PARAMS((int fd, int errflag, void *data));
+
+typedef int (*FILE_WALK_LHD) _PARAMS((int fd, char *buf, int size, void *line_data));
+
+
+
+extern int file_open _PARAMS((char *path, int (*handler) (), int mode));
+extern int file_close _PARAMS((int fd));
+extern int file_write _PARAMS((int fd, char *buf, int len, int access_code,
+	void       (*handle) (), void *handle_data));
+extern int file_write_unlock _PARAMS((int fd, int access_code));
+extern int file_read _PARAMS((int fd, char *buf, int req_len, int offset,
+	int       (*handler) (int fd, char *buf, int size,
+	    int errflag, void *data, int offset),
+	void *client_data));
+extern int file_walk _PARAMS((int fd,
+	int       (*handler) (int fd, int errflag, void *data),
+	void *client_data,
+	int       (*line_handler)
+	          (int fd, char *buf, int size, void *line_data),
+	void *line_data));
+extern int file_get_fd _PARAMS((char *filename));
+extern int file_update_open _PARAMS((int fd, char *path));
+extern int file_write_lock _PARAMS((int fd));
+extern int disk_init _PARAMS((void));
+
+#endif /* DISK_H */
---
+++ tags/BASELINE_1_0_alpha14/src/send-announce.c
@@ -0,0 +1,66 @@
+/* $Id$ */
+
+/*
+ * DEBUG: Section 27          send-announce
+ */
+
+#include "squid.h"
+
+
+void send_announce()
+{
+    static char tbuf[256];
+    static char sndbuf[BUFSIZ];
+    icpUdpData *qdata = NULL;
+    struct hostent *hp = NULL;
+    char *host = NULL;
+    char *file = NULL;
+    int port;
+    int fd;
+    int l;
+    int n;
+
+    sndbuf[0] = '\0';
+
+    sprintf(tbuf, "cache_version HARVEST/%s\n", version_string);
+    strcat(sndbuf, tbuf);
+    sprintf(tbuf, "Running on %s %d %d\n",
+	getMyHostname(),
+	getAsciiPortNum(),
+	getUdpPortNum());
+    strcat(sndbuf, tbuf);
+    sprintf(tbuf, "generated %d [%s]\n",
+	(int) cached_curtime,
+	mkhttpdlogtime(&cached_curtime));
+    strcat(sndbuf, tbuf);
+    l = strlen(sndbuf);
+
+    host = getAnnounceHost();
+    port = getAnnouncePort();
+
+    if ((hp = ipcache_gethostbyname(host)) == NULL) {
+	debug(27, 1, "send_announce: Unknown host '%s'\n", host);
+	return;
+    }
+    if ((file = getAnnounceFile())) {
+	/* XXX could block */
+	fd = file_open(file, NULL, O_RDONLY);
+	if (fd > -1 && (n = read(fd, sndbuf + l, BUFSIZ - l - 1)) > 0) {
+	    l += n;
+	    sndbuf[l] = '\0';
+	} else {
+	    debug(27, 1, "send_announce: %s: %s\n", file, xstrerror());
+	}
+    }
+    qdata = (icpUdpData *) xcalloc(1, sizeof(icpUdpData));
+    qdata->msg = xstrdup(sndbuf);
+    qdata->len = strlen(sndbuf) + 1;
+    qdata->address.sin_family = AF_INET;
+    qdata->address.sin_port = htons(port);
+    memcpy(&qdata->address.sin_addr, hp->h_addr_list[0], hp->h_length);
+    AppendUdp(qdata);
+    comm_set_select_handler(theUdpConnection,
+	COMM_SELECT_WRITE,
+	(PF) icpUdpReply,
+	(void *) qdata);
+}
