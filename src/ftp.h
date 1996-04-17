bfa2552ed933c1d8d791b8f8a7ec816271e7c149
nobody <nobody@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
1996-04-17 21:39:55 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.44.2'.

--- src/ftp.h
+++
@@ -1,6 +0,0 @@
-
-/* $Id$ */
-
-extern int ftpStart _PARAMS((int unusedfd, char *url, StoreEntry * entry));
-extern int ftpInitialize _PARAMS((void));
-extern int ftpCachable _PARAMS((char *));
--- src/gopher.h
+++
@@ -1,3 +0,0 @@
-
-extern int gopherStart _PARAMS((int, char *, StoreEntry *));
-extern int gopherCachable _PARAMS((char *));
