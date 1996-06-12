bbd848a3d9b9bf15df01557d0b73a866a1119b07
nobody <nobody@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
1996-06-12 20:43:29 +0000

    This commit was manufactured by cvs2svn to create tag 'SQUID_1_0_beta17'.

---
+++ tags/SQUID_1_0_beta17/src/icp.h
@@ -0,0 +1,71 @@
+
+/* $Id$ */
+
+#ifndef ICP_H
+#define ICP_H
+
+typedef enum {
+    LOG_TAG_NONE,		/* 0 */
+    LOG_TCP_HIT,		/* 1 */
+    LOG_TCP_MISS,		/* 2 */
+    LOG_TCP_EXPIRED,		/* 3 */
+    LOG_TCP_USER_REFRESH,	/* 4 */
+    LOG_TCP_IFMODSINCE,		/* 5 */
+    LOG_TCP_SWAPIN_FAIL,	/* 6 */
+    LOG_TCP_DENIED,		/* 7 */
+    LOG_UDP_HIT,		/* 8 */
+    LOG_UDP_HIT_OBJ,		/* 9 */
+    LOG_UDP_MISS,		/* 10 */
+    LOG_UDP_DENIED,		/* 11 */
+    LOG_UDP_INVALID,		/* 12 */
+    ERR_READ_TIMEOUT,		/* 13 */
+    ERR_LIFETIME_EXP,		/* 14 */
+    ERR_NO_CLIENTS_BIG_OBJ,	/* 15 */
+    ERR_READ_ERROR,		/* 16 */
+    ERR_CLIENT_ABORT,		/* 17 */
+    ERR_CONNECT_FAIL,		/* 18 */
+    ERR_INVALID_REQ,		/* 19 */
+    ERR_INVALID_URL,		/* 20 */
+    ERR_NO_FDS,			/* 21 */
+    ERR_DNS_FAIL,		/* 22 */
+    ERR_NOT_IMPLEMENTED,	/* 23 */
+    ERR_CANNOT_FETCH,		/* 24 */
+    ERR_NO_RELAY,		/* 25 */
+    ERR_DISK_IO,		/* 26 */
+    ERR_ZERO_SIZE_OBJECT	/* 27 */
+} log_type;
+
+#define ERR_MIN ERR_READ_TIMEOUT
+#define ERR_MAX ERR_ZERO_SIZE_OBJECT
+
+/* bitfields for the icpStateData 'flags' element */
+#define		REQ_HTML	0x01
+#define		REQ_NOCACHE	0x02
+#define		REQ_IMS		0x04
+#define		REQ_AUTH	0x08
+#define		REQ_CACHABLE	0x10
+#define 	REQ_ACCEL	0x20
+#define 	REQ_HIERARCHICAL 0x40
+#define 	REQ_LOOPDETECT  0x80
+
+typedef struct wwd {
+    struct sockaddr_in address;
+    char *msg;
+    long len;
+    struct wwd *next;
+    struct timeval start;
+    log_type logcode;
+} icpUdpData;
+
+extern char *icpWrite _PARAMS((int, char *, int, int, void (*handler) (), void *));
+extern int icpUdpSend _PARAMS((int, char *, icp_common_t *, struct sockaddr_in *, icp_opcode, log_type));
+
+extern int icpHandleUdp _PARAMS((int sock, void *data));
+extern int asciiHandleConn _PARAMS((int sock, void *data));
+extern void AppendUdp _PARAMS((icpUdpData *));
+
+extern int neighbors_do_private_keys;
+extern char *IcpOpcodeStr[];
+extern int icpUdpReply _PARAMS((int fd, icpUdpData * queue));
+
+#endif
---
+++ tags/SQUID_1_0_beta17/src/stat.h
@@ -0,0 +1,138 @@
+/* $Id$ */
+
+#ifndef STAT_H
+#define STAT_H
+
+/* logfile status */
+#define LOG_ENABLE  1
+#define LOG_DISABLE 0
+
+typedef struct _proto_stat {
+    char protoname[25];
+    int object_count;
+
+    struct _usage {
+	int max;
+	int avg;
+	int min;
+	int now;
+    } kb;
+
+    unsigned int hit;
+    unsigned int miss;
+    float hitratio;
+    unsigned int transferrate;
+    unsigned int refcount;
+    unsigned int transferbyte;
+
+} proto_stat;
+
+typedef struct _meta_data_stat {
+    int hot_vm;
+    int store_entries;
+    int store_in_mem_objects;
+    int ipcache_count;
+    int hash_links;
+    int url_strings;
+} Meta_data;
+
+extern Meta_data meta_data;
+
+struct _cacheinfo {
+
+    /* information retrieval method */
+    /* get a processed statistic object */
+    void (*stat_get) _PARAMS((struct _cacheinfo * c, char *req, StoreEntry * sentry));
+
+    /* get a processed info object */
+    void (*info_get) _PARAMS((struct _cacheinfo * c, StoreEntry * sentry));
+
+    /* get a processed logfile object */
+    void (*log_get_start) _PARAMS((struct _cacheinfo * c, StoreEntry * sentry));
+
+    /* get a processed logfile status */
+    void (*log_status_get) _PARAMS((struct _cacheinfo * c, StoreEntry * sentry));
+
+    /* get a processed squid.conf object */
+    void (*squid_get_start) _PARAMS((struct _cacheinfo * c, StoreEntry * sentry));
+
+    /* get a parameter object */
+    void (*parameter_get) _PARAMS((struct _cacheinfo * c, StoreEntry * sentry));
+    void (*server_list) _PARAMS((struct _cacheinfo * c, StoreEntry * sentry));
+
+
+    /* get a total bytes for object in cache */
+    int (*cache_size_get) _PARAMS((struct _cacheinfo * c));
+
+    /* statistic update method */
+
+    /* add a transaction to system log */
+    void (*log_append) _PARAMS((struct _cacheinfo * obj, char *url, char *id,
+	    int size, char *action, char *method, int http_code, int msec, hier_code));
+
+    /* clear logfile */
+    void (*log_clear) _PARAMS((struct _cacheinfo * obj, StoreEntry * sentry));
+
+    /* enable logfile */
+    void (*log_enable) _PARAMS((struct _cacheinfo * obj, StoreEntry * sentry));
+
+    /* disable logfile */
+    void (*log_disable) _PARAMS((struct _cacheinfo * obj, StoreEntry * sentry));
+
+    /* protocol specific stat update method */
+    /* return a proto_id for a given url */
+         protocol_t(*proto_id) _PARAMS((char *url));
+
+    /* a new object cached. update obj count, size */
+    void (*proto_newobject) _PARAMS((struct _cacheinfo * c, protocol_t proto_id, int len, int flag));
+
+    /* an object purged */
+    void (*proto_purgeobject) _PARAMS((struct _cacheinfo * c, protocol_t proto_id, int len));
+
+    /* an object is referred to. */
+    void (*proto_touchobject) _PARAMS((struct _cacheinfo * c, protocol_t proto_id, int len));
+
+    /* a hit. update hit count, transfer byted. refcount */
+    void (*proto_hit) _PARAMS((struct _cacheinfo * obj, protocol_t proto_id));
+
+    /* a miss. update miss count. refcount */
+    void (*proto_miss) _PARAMS((struct _cacheinfo * obj, protocol_t proto_id));
+
+    /* dummy Notimplemented object handler */
+    void (*NotImplement) _PARAMS((struct _cacheinfo * c, StoreEntry * sentry));
+
+    /* stat table and data */
+    char logfilename[256];	/* logfile name */
+    int logfile_fd;		/* logfile fd */
+    int logfile_access;		/* logfile access code */
+    /* logfile status {enable, disable} */
+    int logfile_status;
+
+    /* protocol stat data */
+    proto_stat proto_stat_data[PROTO_MAX + 1];
+
+};
+
+struct _iostats {
+    struct {
+	int reads;
+	int reads_deferred;
+	int read_hist[16];
+	int writes;
+	int write_hist[16];
+    } Http, Ftp;
+};
+
+extern struct _iostats IOStats;
+
+extern cacheinfo *CacheInfo;
+extern unsigned long ntcpconn;
+extern unsigned long nudpconn;
+extern char *open_bracket;
+extern char *close_bracket;
+
+extern void stat_init _PARAMS((cacheinfo **, char *));
+extern void stat_rotate_log _PARAMS((void));
+
+
+#endif /*STAT_H */
