54034909b3a0ed57bb8f7fbd9a2996ad306c7fc9
nobody <nobody@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
1996-09-05 19:04:47 +0000

    This commit was manufactured by cvs2svn to create tag 'SQUID_1_1_alpha17'.

---
+++ tags/SQUID_1_1_alpha17/acconfig.h
@@ -0,0 +1,34 @@
+/* $Id$ */
+
+/* Define if struct tm has tm_gmtoff member */
+#undef HAVE_TM_GMTOFF
+
+/* Define if struct mallinfo has mxfast member */
+#undef HAVE_EXT_MALLINFO
+
+/* Maximum number of open filedescriptors */
+#undef SQUID_FD_SETSIZE
+
+/* UDP send buffer size */
+#undef SQUID_UDP_SO_SNDBUF
+
+/* UDP receive buffer size */
+#undef SQUID_UDP_SO_RCVBUF
+
+/* TCP send buffer size */
+#undef SQUID_TCP_SO_SNDBUF
+
+/* TCP receive buffer size */
+#undef SQUID_TCP_SO_RCVBUF
+
+/* Host type from configure */
+#undef CONFIG_HOST_TYPE
+
+/* If we need to declare sys_errlist[] as external */
+#undef NEED_SYS_ERRLIST
+
+/* If gettimeofday is known to take only one argument */
+#undef GETTIMEOFDAY_NO_TZP
+
+/* If libresolv.a has been hacked to export _dns_ttl_ */
+#undef LIBRESOLV_DNS_TTL_HACK
