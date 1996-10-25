1457560bd0d4148c7fcf8f34cc2e998610916d60
nobody <nobody@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
1996-10-25 03:47:36 +0000

    This commit was manufactured by cvs2svn to create tag 'SQUID_1_1_beta10'.

---
+++ tags/SQUID_1_1_beta10/include/ansiproto.h
@@ -0,0 +1,7 @@
+#ifndef _PARAMS
+#if defined(__STDC__) || defined(__cplusplus)
+#define _PARAMS(ARGS) ARGS
+#else /* Traditional C */
+#define _PARAMS(ARGS) ()
+#endif /* __STDC__ */
+#endif /* _PARAMS */
