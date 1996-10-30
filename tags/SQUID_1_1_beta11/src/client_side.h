4141739c73c0bade957832837c66e72c0122ba52
nobody <nobody@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
1996-10-30 09:28:04 +0000

    This commit was manufactured by cvs2svn to create tag 'SQUID_1_1_beta11'.

---
+++ tags/SQUID_1_1_beta11/src/client_side.h
@@ -0,0 +1,7 @@
+extern void clientAccessCheck(icpStateData *, void (*)_PARAMS((icpStateData *, int)));
+extern void clientAccessCheckDone _PARAMS((icpStateData *, int answer));
+extern int icpProcessExpired _PARAMS((int fd, icpStateData *));
+
+#if USE_PROXY_AUTH
+char *proxyAuthenticate(char *headers);
+#endif /* USE_PROXY_AUTH */
