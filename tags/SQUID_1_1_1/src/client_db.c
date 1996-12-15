e3be83e80f6d08342207701458721360a38e5dfe
nobody <nobody@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
1996-12-15 23:35:52 +0000

    This commit was manufactured by cvs2svn to create tag 'SQUID_1_1_1'.

---
+++ tags/SQUID_1_1_1/src/client_db.c
@@ -0,0 +1,131 @@
+
+/*
+ * $Id$
+ *
+ * DEBUG: section 0     Client Database
+ * AUTHOR: Duane Wessels
+ *
+ * SQUID Internet Object Cache  http://squid.nlanr.net/Squid/
+ * --------------------------------------------------------
+ *
+ *  Squid is the result of efforts by numerous individuals from the
+ *  Internet community.  Development is led by Duane Wessels of the
+ *  National Laboratory for Applied Network Research and funded by
+ *  the National Science Foundation.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ */
+
+#include "squid.h"
+
+typedef struct _client_info {
+    char *key;
+    struct client_info *next;
+    struct in_addr addr;
+    int result_hist[ERR_MAX];
+    int n_http;
+    int n_icp;
+    int n_requests;
+} ClientInfo;
+
+int client_info_sz;
+
+static HashID client_table = 0;
+
+static ClientInfo *clientdbAdd _PARAMS((struct in_addr addr));
+
+static ClientInfo *
+clientdbAdd(struct in_addr addr)
+{
+    ClientInfo *c;
+    c = xcalloc(1, sizeof(ClientInfo));
+    c->key = xstrdup(inet_ntoa(addr));
+    c->addr = addr;
+    hash_join(client_table, (hash_link *) c);
+    meta_data.client_info++;
+    return c;
+}
+
+void
+clientdbInit(void)
+{
+    if (client_table)
+	return;
+    client_table = hash_create((int (*)_PARAMS((const char *, const char *))) strcmp,
+	229,
+	hash_string);
+    client_info_sz = sizeof(ClientInfo);
+}
+
+void
+clientdbUpdate(struct in_addr addr, log_type log_type, u_short port)
+{
+    char *key = inet_ntoa(addr);
+    ClientInfo *c = (ClientInfo *) hash_lookup(client_table, key);
+    if (c == NULL)
+	c = clientdbAdd(addr);
+    if (c == NULL)
+	debug_trap("clientdbUpdate: Failed to add entry");
+    c->result_hist[log_type]++;
+    if (port == Config.Port.http)
+	c->n_http++;
+    else if (port == Config.Port.icp)
+	c->n_icp++;
+    c->n_requests++;
+}
+
+int
+clientdbDeniedPercent(struct in_addr addr)
+{
+    char *key = inet_ntoa(addr);
+    int n = 100;
+    ClientInfo *c = (ClientInfo *) hash_lookup(client_table, key);
+    if (c == NULL)
+	return 0;
+    if (c->n_icp > 100)
+	n = c->n_icp;
+    return 100 * c->result_hist[ICP_OP_DENIED] / n;
+}
+
+void
+clientdbDump(StoreEntry * sentry)
+{
+    ClientInfo *c;
+    log_type l;
+    storeAppendPrintf(sentry, "{Cache Clients:\n");
+    c = (ClientInfo *) hash_first(client_table);
+    while (c) {
+	storeAppendPrintf(sentry, "{Address: %s}\n", c->key);
+	storeAppendPrintf(sentry, "{Name: %s}\n", fqdnFromAddr(c->addr));
+	storeAppendPrintf(sentry, "{    HTTP Requests %d}\n",
+	    c->n_http);
+	storeAppendPrintf(sentry, "{    ICP Requests %d}\n",
+	    c->n_icp);
+	storeAppendPrintf(sentry, "{    Log Code Histogram:}\n");
+	for (l = LOG_TAG_NONE; l < ERR_MAX; l++) {
+	    if (c->result_hist[l] == 0)
+		continue;
+	    storeAppendPrintf(sentry,
+		"{        %-20.20s %7d %3d%%}\n",
+		log_tags[l],
+		c->result_hist[l],
+		percent(c->result_hist[l], c->n_requests));
+	}
+	storeAppendPrintf(sentry, "{}\n");
+	c = (ClientInfo *) hash_next(client_table);
+    }
+    storeAppendPrintf(sentry, close_bracket);
+}
