e3e5820617fad8a4a466e69ead1fbc136c1d26ac
nobody <nobody@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
1998-01-06 22:58:08 +0000

    This commit was manufactured by cvs2svn to create tag 'SQUID_1_2_beta11'.

---
+++ tags/SQUID_1_2_beta11/src/fd.c
@@ -0,0 +1,119 @@
+
+/*
+ * $Id$
+ *
+ * DEBUG: section 51    Filedescriptor Functions
+ * AUTHOR: Duane Wessels
+ *
+ * SQUID Internet Object Cache  http://squid.nlanr.net/Squid/
+ * --------------------------------------------------------
+ *
+ *  Squid is the result of efforts by numerous individuals from the
+ *  Internet community.  Development is led by Duane Wessels of the
+ *  National Laboratory for Applied Network Research and funded by
+ *  the National Science Foundation.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ */
+
+#include "squid.h"
+
+static void fdUpdateBiggest(int fd, unsigned int status);
+
+static void
+fdUpdateBiggest(int fd, unsigned int status)
+{
+    if (fd < Biggest_FD)
+	return;
+    assert(fd < Squid_MaxFD);
+    if (fd > Biggest_FD) {
+	assert(status == FD_OPEN);
+	Biggest_FD = fd;
+	return;
+    }
+    /* if we are here, then fd == Biggest_FD */
+    assert(status == FD_CLOSE);
+    while (fd_table[Biggest_FD].open != FD_OPEN)
+	Biggest_FD--;
+}
+
+void
+fd_close(int fd)
+{
+    fde *F = &fd_table[fd];
+    if (F->type == FD_FILE) {
+	assert(F->read_handler == NULL);
+	assert(F->write_handler == NULL);
+    }
+    fdUpdateBiggest(fd, F->open = FD_CLOSE);
+    Number_FD--;
+    memset(F, '\0', sizeof(fde));
+    F->timeout = 0;
+}
+
+void
+fd_open(int fd, unsigned int type, const char *desc)
+{
+    fde *F = &fd_table[fd];
+    assert(fd >= 0);
+    assert(F->open == 0);
+    F->type = type;
+    fdUpdateBiggest(fd, F->open = FD_OPEN);
+    if (desc)
+	xstrncpy(F->desc, desc, FD_DESC_SZ);
+    Number_FD++;
+}
+
+void
+fd_note(int fd, const char *s)
+{
+    fde *F = &fd_table[fd];
+    xstrncpy(F->desc, s, FD_DESC_SZ);
+}
+
+void
+fd_bytes(int fd, int len, unsigned int type)
+{
+    fde *F = &fd_table[fd];
+    if (len < 0)
+	return;
+    assert(type == FD_READ || type == FD_WRITE);
+    if (type == FD_READ)
+	F->bytes_read += len;
+    else
+	F->bytes_written += len;
+}
+
+void
+fdFreeMemory(void)
+{
+    safe_free(fd_table);
+}
+
+void
+fdDumpOpen(void)
+{
+    int i;
+    fde *F;
+    for (i = 0; i < Squid_MaxFD; i++) {
+	F = &fd_table[i];
+	if (!F->open)
+	    continue;
+	if (i == fileno(debug_log))
+	    continue;
+	debug(51, 1) ("Open FD %4d %s\n", i, F->desc);
+    }
+}
---
+++ tags/SQUID_1_2_beta11/src/globals.h
@@ -0,0 +1,114 @@
+
+/*
+ * $Id$
+ */
+
+extern FILE *debug_log;		/* NULL */
+extern FILE *cache_useragent_log;	/* NULL */
+extern Meta_data meta_data;
+extern SquidConfig Config;
+extern SquidConfig2 Config2;
+extern cacheinfo *HTTPCacheInfo;
+extern cacheinfo *ICPCacheInfo;
+extern char *ConfigFile;	/* NULL */
+extern char *IcpOpcodeStr[];
+extern char *dns_error_message;	/* NULL */
+extern const char *log_tags[];
+extern char tmp_error_buf[ERROR_BUF_SZ];
+extern char *volatile debug_options;	/* NULL */
+extern char ThisCache[SQUIDHOSTNAMELEN << 1];
+extern char config_input_line[BUFSIZ];
+extern const char *AclMatchedName;	/* NULL */
+extern const char *DefaultConfigFile;	/* DEFAULT_CONFIG_FILE */
+extern const char *RequestMethodStr[];
+extern const char *ProtocolStr[];
+extern const char *cfg_filename;	/* NULL */
+extern const char *const appname;	/* "squid" */
+extern const char *const close_bracket;		/* "}\n" */
+extern const char *const dash_str;	/* "-" */
+extern const char *const localhost;	/* "127.0.0.1" */
+extern const char *const null_string;	/* "" */
+extern const char *const open_bracket;	/* "{\n" */
+extern const char *const version_string;	/* SQUID_VERSION */
+extern const char *const w_space;	/* " \t\n\r" */
+extern const char *fdstatTypeStr[];
+extern const char *hier_strings[];
+extern const char *memStatusStr[];
+extern const char *pingStatusStr[];
+extern const char *storeStatusStr[];
+extern const char *swapStatusStr[];
+extern dnsStatData DnsStats;
+extern fde *fd_table;		/* NULL */
+extern int Biggest_FD;		/* -1 */
+extern int Number_FD;		/* 0 */
+extern int HttpSockets[MAXHTTPPORTS];
+extern int NDnsServersAlloc;	/* 0 */
+extern int NHttpSockets;	/* 0 */
+extern int RESERVED_FD;
+extern int Squid_MaxFD;		/* SQUID_MAXFD */
+extern int config_lineno;	/* 0 */
+extern int configured_once;	/* 0 */
+extern int debugLevels[MAX_DEBUG_SECTIONS];
+extern int do_mallinfo;		/* 0 */
+extern int do_reuse;		/* 1 */
+extern int hash_links_allocated;
+extern int icmp_sock;		/* -1 */
+extern int neighbors_do_private_keys;	/* 1 */
+extern int opt_accel_uses_host;	/* 0 */
+extern int opt_catch_signals;	/* 1 */
+extern int opt_debug_stderr;	/* 0 */
+extern int opt_dns_tests;	/* 1 */
+extern int opt_foreground_rebuild;	/* 0 */
+extern int opt_forwarded_for;	/* 1 */
+extern int opt_mem_pools;	/* 1 */
+extern int opt_reload_hit_only;	/* 0 */
+extern int opt_syslog_enable;	/* 0 */
+extern int opt_udp_hit_obj;	/* 0 */
+extern int opt_create_swap_dirs;	/* 0 */
+extern int syslog_enable;	/* 0 */
+extern int theInIcpConnection;	/* -1 */
+extern int theOutIcpConnection;	/* -1 */
+#ifdef SQUID_SNMP
+extern int theInSnmpConnection;	/* -1 */
+extern int theOutSnmpConnection;	/* -1 */
+extern char *snmp_agentinfo;
+#endif
+extern int vhost_mode;		/* 0 */
+extern int vizSock;		/* -1 */
+extern iostats IOStats;
+extern stmem_stats disk_stats;
+extern stmem_stats mem_obj_pool;
+extern stmem_stats request_pool;
+extern stmem_stats sm_stats;
+extern struct _acl_deny_info_list *DenyInfoList;	/* NULL */
+extern struct in_addr any_addr;
+extern struct in_addr local_addr;
+extern struct in_addr no_addr;
+extern struct in_addr theOutICPAddr;
+extern struct in_addr theOutSNMPAddr;
+extern struct timeval current_time;
+extern struct timeval squid_start;
+extern time_t squid_curtime;	/* 0 */
+extern volatile int reconfigure_pending;	/* 0 */
+extern volatile int shutdown_pending;	/* 0 */
+extern int fileno_stack_count;	/* 0 */
+extern int store_rebuilding;	/* 1 */
+extern int store_swap_size;	/* 0 */
+extern int client_info_sz;	/* 0 */
+extern unsigned long store_mem_size;	/* 0 */
+extern icpUdpData *UdpQueueHead;	/* NULL */
+extern icpUdpData *UdpQueueTail;	/* NULL */
+extern time_t hit_only_mode_until;	/* 0 */
+extern StatCounters Counter;
+extern char *err_type_str[];
+extern char *icp_opcode_str[];
+extern struct radix_node_head *AS_tree_head;
+extern double request_failure_ratio; /* 0.0 */
+
+#ifdef HAVE_SYSLOG
+extern int _db_level;
+#endif
+
+#if SQUID_SNMP
+extern struct tree *Mib;
+#endif
