4ee6a4d6fd8c2b97aeeca6624911d9f671eed5d1
rousskov <rousskov@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
1998-02-10 21:34:38 +0000

    Added HttpHeader object (initial revision)

---
+++ branches/alex_2/src/HttpHeader.h
@@ -0,0 +1,91 @@
+/*
+ * $Id$
+ *
+ * AUTHOR: Alex Rousskov
+ *
+ * SQUID Internet Object Cache  http://squid.nlanr.net/Squid/
+ * --------------------------------------------------------
+ *
+ *  Squid is the result of efforts by numerous individuals from the
+ *  Internet community.  Development is led by Duane Wessels of the
+ *  National Laboratory for Applied Network Research and funded by
+ *  the National Science Foundation.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ */
+
+#ifndef _HTTP_HEADER_H_
+#define _HTTP_HEADER_H_
+
+struct _HttpHeaderField {
+	char *name;   /* field-name from HTTP/1.1 (no column!) */
+	char *value;  /* field-value from HTTP/1.1 */
+};
+
+struct _HttpHeader {
+	/* public, read only */
+	size_t packed_size;  /* packed header size (see httpHeaderPack()) */
+
+	/* protected, do not use these, use interface functions instead */
+	int count;        /* #headers */
+	int capacity;     /* max #headers before we have to grow */
+	struct _HttpHeaderField **fields;
+};
+
+/*
+ * use HttpHeaderPos as opaque type, do not interpret, 
+ * it is not what you think it is
+ */
+typedef size_t HttpHeaderPos; 
+
+/* use this and only this to initialize HttpHeaderPos */
+#define HttpHeaderInitPos (-1)
+
+/* create/init/destroy */
+extern HttpHeader *httpHeaderCreate();
+extern void httpHeaderInit(HttpHeader *hdr);
+extern void httpHeaderDestroy(HttpHeader *hdr);
+
+/* parse/pack */
+/* parse a 0-terminating buffer and fill internal structires; _end points at the first character after the header; returns true if successfull */
+extern int httpHeaderParse(HttpHeader *hdr, const char *header_start, const char *header_end);
+/* pack header into the buffer, does not check for overflow, check hdr.packed_size first! */
+extern void httpHeaderPack(const HttpHeader *hdr, char *buf);
+
+/* iterate through fields with name (or find first field with name) */
+extern const char *httpHeaderGetStr(const HttpHeader *hdr, const char *name, HttpHeaderPos *pos);
+extern long httpHeaderGetInt(const HttpHeader *hdr, const char *name, HttpHeaderPos *pos);
+
+/* iterate through all fields */
+extern HttpHeaderField *httpHeaderGetField(const HttpHeader *hdr, const char **name, const char **value, HttpHeaderPos *pos);
+
+/* delete field(s) by name or pos */
+extern int httpHeaderDelFields(HttpHeader *hdr, const char *name);
+extern void httpHeaderDelField(HttpHeader *hdr, HttpHeaderPos pos);
+
+/* add a field (appends) */
+extern const char *httpHeaderAddStrField(HttpHeader *hdr, const char *name, const char *value);
+extern long httpHeaderAddIntField(HttpHeader *hdr, const char *name, long value);
+
+/* often used field names (may use caching to speedup retreival!) */
+extern size_t httpHeaderGetContentLength(const HttpHeader *hdr);
+extern time_t httpHeaderGetMaxAge(const HttpHeader *hdr);
+extern int httpHeaderGetMaxForward(const HttpHeader *hdr);
+
+/* put report about current header usage and other stats into a static string */
+extern const char *httpHeaderReport();
+
+#endif /* ndef _HTTP_HEADER_H_ */
