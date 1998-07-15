ea7388c7bcc83b85980ce51f7ab3dc0bdfed4419
wessels <wessels@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
1998-07-15 22:14:20 +0000

    Tim Steele tjfs@tadpole.co.uk says fix this

--- branches/SQUID_1_1_branch/src/stat.c
+++ branches/SQUID_1_1_branch/src/stat.c
@@ -573,7 +573,8 @@ server_list(const cacheinfo * obj, StoreEntry * sentry)
 	}
 	storeAppendPrintf(sentry, "{Status     : %s}\n",
 	    neighborUp(e) ? "Up" : "Down");
-	storeAppendPrintf(sentry, "{AVG RTT    : %d msec}\n", e->stats.rtt);
+	storeAppendPrintf(sentry, "{AVG RTT    : %d msec}\n",
+		e->stats.rtt & 0xFFFF);
 	storeAppendPrintf(sentry, "{ACK DEFICIT: %8d}\n", e->stats.ack_deficit);
 	storeAppendPrintf(sentry, "{PINGS SENT : %8d}\n", e->stats.pings_sent);
 	storeAppendPrintf(sentry, "{PINGS ACKED: %8d %3d%%}\n",
