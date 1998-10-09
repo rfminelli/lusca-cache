04adb62fcd7c31e0db623115b85d2b8c634512a4
nobody <nobody@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
1998-10-09 17:10:24 +0000

    This commit was manufactured by cvs2svn to create tag 'SQUID_2_0_PATCH1'.

---
+++ tags/SQUID_2_0_PATCH1/include/splay.h
@@ -0,0 +1,21 @@
+/*
+ * $Id$
+ */
+
+
+typedef struct _splay_node {
+    void *data;
+    struct _splay_node *left;
+    struct _splay_node *right;
+} splayNode;
+
+typedef int SPLAYCMP(const void *, splayNode *);
+typedef void SPLAYWALKEE(void *, void *);
+typedef void SPLAYFREE(void *);
+
+extern int splayLastResult;
+
+extern splayNode *splay_insert(void *, splayNode *, SPLAYCMP *);
+extern splayNode *splay_splay(const void *, splayNode *, SPLAYCMP *);
+extern void splay_destroy(splayNode *, SPLAYFREE *);
+extern void splay_walk(splayNode *, SPLAYWALKEE *, void *);
---
+++ tags/SQUID_2_0_PATCH1/include/version.h
@@ -0,0 +1,12 @@
+/*
+ * $Id$
+ *
+ *  SQUID_VERSION - String for version id of this distribution
+ */
+#ifndef SQUID_VERSION
+#define SQUID_VERSION	"2.0.PATCH1"
+#endif
+
+#ifndef SQUID_RELEASE_TIME
+#define SQUID_RELEASE_TIME squid_curtime
+#endif
