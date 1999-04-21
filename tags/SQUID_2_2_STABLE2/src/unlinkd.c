72e4ffa2be657748a2088f83d913dcea831dfcef
nobody <nobody@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
1999-04-21 06:10:29 +0000

    This commit was manufactured by cvs2svn to create tag 'SQUID_2_2_STABLE2'.

---
+++ tags/SQUID_2_2_STABLE2/src/unlinkd.c
@@ -0,0 +1,145 @@
+/*
+ * $Id$
+ *
+ * DEBUG: section 12    Unlink Daemon
+ * AUTHOR: Duane Wessels
+ *
+ * SQUID Internet Object Cache  http://squid.nlanr.net/Squid/
+ * ----------------------------------------------------------
+ *
+ *  Squid is the result of efforts by numerous individuals from the
+ *  Internet community.  Development is led by Duane Wessels of the
+ *  National Laboratory for Applied Network Research and funded by the
+ *  National Science Foundation.  Squid is Copyrighted (C) 1998 by
+ *  Duane Wessels and the University of California San Diego.  Please
+ *  see the COPYRIGHT file for full details.  Squid incorporates
+ *  software developed and/or copyrighted by other sources.  Please see
+ *  the CREDITS file for full details.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+ *
+ */
+
+#include "squid.h"
+
+#ifdef UNLINK_DAEMON
+
+/* This is the external unlinkd process */
+
+#define UNLINK_BUF_LEN 1024
+
+int
+main(int argc, char *argv[])
+{
+    char buf[UNLINK_BUF_LEN];
+    char *t;
+    setbuf(stdin, NULL);
+    while (fgets(buf, UNLINK_BUF_LEN, stdin)) {
+	if ((t = strchr(buf, '\n')))
+	    *t = '\0';
+#if USE_TRUNCATE_NOT_UNLINK
+	truncate(buf, 0);
+#else
+	unlink(buf);
+#endif
+    }
+    exit(0);
+}
+
+#else /* UNLINK_DAEMON */
+
+/* This code gets linked to Squid */
+
+#if USE_UNLINKD
+static int unlinkd_wfd = -1;
+static int unlinkd_rfd = -1;
+#endif
+
+void
+unlinkdUnlink(const char *path)
+{
+#if USE_UNLINKD
+    char *buf;
+    int l;
+    if (unlinkd_wfd < 0) {
+	debug_trap("unlinkdUnlink: unlinkd_wfd < 0");
+	safeunlink(path, 0);
+	return;
+    }
+    l = strlen(path) + 1;
+    buf = xcalloc(1, l + 1);
+    strcpy(buf, path);
+    strcat(buf, "\n");
+    file_write(unlinkd_wfd,
+	-1,
+	buf,
+	l,
+	NULL,			/* Handler */
+	NULL,			/* Handler-data */
+	xfree);
+    Counter.unlink.requests++;
+#endif
+}
+
+void
+unlinkdClose(void)
+{
+#if USE_UNLINKD
+    assert(unlinkd_wfd > -1);
+    debug(12, 1) ("Closing unlinkd pipe on FD %d\n", unlinkd_wfd);
+    file_close(unlinkd_wfd);
+    if (unlinkd_wfd != unlinkd_rfd)
+	file_close(unlinkd_rfd);
+    unlinkd_wfd = -1;
+    unlinkd_rfd = -1;
+#endif
+}
+
+void
+unlinkdInit(void)
+{
+#if USE_UNLINKD
+    int x;
+    char *args[2];
+    struct timeval slp;
+    args[0] = "(unlinkd)";
+    args[1] = NULL;
+#if HAVE_POLL && defined(_SQUID_OSF_)
+    /* pipes and poll() don't get along on DUNIX -DW */
+    x = ipcCreate(IPC_TCP_SOCKET,
+#else
+    x = ipcCreate(IPC_FIFO,
+#endif
+	Config.Program.unlinkd,
+	args,
+	"unlinkd",
+	&unlinkd_rfd,
+	&unlinkd_wfd);
+    if (x < 0)
+	fatal("Failed to create unlinkd subprocess");
+    slp.tv_sec = 0;
+    slp.tv_usec = 250000;
+    select(0, NULL, NULL, NULL, &slp);
+    fd_note(unlinkd_wfd, "squid -> unlinkd");
+    fd_note(unlinkd_rfd, "unlinkd -> squid");
+    commSetTimeout(unlinkd_rfd, -1, NULL, NULL);
+    commSetTimeout(unlinkd_wfd, -1, NULL, NULL);
+    commSetNonBlocking(unlinkd_wfd);
+    commSetNonBlocking(unlinkd_rfd);
+    debug(12, 1) ("Unlinkd pipe opened on FD %d\n", unlinkd_wfd);
+#endif
+}
+
+#endif /* ndef UNLINK_DAEMON */
