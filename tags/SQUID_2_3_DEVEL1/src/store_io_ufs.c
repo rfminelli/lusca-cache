2aeaf881bb9b97effec6b75b0f7e1d1b68f9f82c
nobody <nobody@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
1999-07-12 23:07:48 +0000

    This commit was manufactured by cvs2svn to create tag 'SQUID_2_3_DEVEL1'.

---
+++ tags/SQUID_2_3_DEVEL1/src/store_io_ufs.c
@@ -0,0 +1,156 @@
+
+/*
+ * DEBUG 78
+ */
+
+#include "squid.h"
+
+#define SWAP_DIR_SHIFT 24
+#define SWAP_FILE_MASK 0x00FFFFFF
+
+static DRCB storeUfsReadDone;
+static DWCB storeUfsWriteDone;
+static void storeUfsIOCallback(storeIOState * sio, int errflag);
+
+/* === PUBLIC =========================================================== */
+
+storeIOState *
+storeUfsOpen(sfileno f, mode_t mode, STIOCB * callback, void *callback_data)
+{
+    char *path = storeUfsFullPath(f, NULL);
+    storeIOState *sio;
+    struct stat sb;
+    int fd;
+    debug(78, 3) ("storeUfsOpen: fileno %08X, mode %d\n", f, mode);
+    assert(mode == O_RDONLY || mode == O_WRONLY);
+    sio = memAllocate(MEM_STORE_IO);
+    cbdataAdd(sio, memFree, MEM_STORE_IO);
+    sio->type.ufs.fd = -1;
+    sio->swap_file_number = f;
+    sio->mode = mode;
+    sio->callback = callback;
+    sio->callback_data = callback_data;
+    if (mode == O_WRONLY)
+	mode |= (O_CREAT | O_TRUNC);
+    fd = file_open(path, mode);
+    debug(78, 3) ("storeUfsOpen: opened FD %d\n", fd);
+    sio->type.ufs.flags.writing = 0;
+    if (fd < 0) {
+	debug(78, 3) ("storeUfsOpenDone: got failure (%d)\n", errno);
+	return NULL;
+    }
+    sio->type.ufs.fd = fd;
+    if (sio->mode == O_RDONLY)
+	if (fstat(fd, &sb) == 0)
+	    sio->st_size = sb.st_size;
+    store_open_disk_fd++;
+    return sio;
+}
+
+void
+storeUfsClose(storeIOState * sio)
+{
+    debug(78, 3) ("storeUfsClose: fileno %08X, FD %d\n",
+	sio->swap_file_number, sio->type.ufs.fd);
+    if (sio->type.ufs.flags.reading || sio->type.ufs.flags.writing) {
+	sio->type.ufs.flags.close_request = 1;
+	return;
+    }
+    storeUfsIOCallback(sio, 0);
+}
+
+void
+storeUfsRead(storeIOState * sio, char *buf, size_t size, off_t offset, STRCB * callback, void *callback_data)
+{
+    assert(sio->read.callback == NULL);
+    assert(sio->read.callback_data == NULL);
+    sio->read.callback = callback;
+    sio->read.callback_data = callback_data;
+    cbdataLock(callback_data);
+    debug(78, 3) ("storeUfsRead: fileno %08X, FD %d\n",
+	sio->swap_file_number, sio->type.ufs.fd);
+    sio->offset = offset;
+    sio->type.ufs.flags.reading = 1;
+    file_read(sio->type.ufs.fd,
+	buf,
+	size,
+	offset,
+	storeUfsReadDone,
+	sio);
+}
+
+void
+storeUfsWrite(storeIOState * sio, char *buf, size_t size, off_t offset, FREE * free_func)
+{
+    debug(78, 3) ("storeUfsWrite: fileno %08X, FD %d\n", sio->swap_file_number, sio->type.ufs.fd);
+    sio->type.ufs.flags.writing = 1;
+    file_write(sio->type.ufs.fd,
+	offset,
+	buf,
+	size,
+	storeUfsWriteDone,
+	sio,
+	free_func);
+}
+
+void
+storeUfsUnlink(sfileno f)
+{
+    debug(78, 3) ("storeUfsUnlink: fileno %08X\n", f);
+    unlinkdUnlink(storeUfsFullPath(f, NULL));
+}
+
+/*  === STATIC =========================================================== */
+
+static void
+storeUfsReadDone(int fd, const char *buf, int len, int errflag, void *my_data)
+{
+    storeIOState *sio = my_data;
+    STRCB *callback = sio->read.callback;
+    void *their_data = sio->read.callback_data;
+    debug(78, 3) ("storeUfsReadDone: fileno %08X, FD %d, len %d\n",
+	sio->swap_file_number, fd, len);
+    sio->type.ufs.flags.reading = 0;
+    if (errflag) {
+	debug(78, 3) ("storeUfsReadDone: got failure (%d)\n", errflag);
+	storeUfsIOCallback(sio, errflag);
+	return;
+    }
+    sio->offset += len;
+    assert(callback);
+    assert(their_data);
+    sio->read.callback = NULL;
+    sio->read.callback_data = NULL;
+    if (cbdataValid(their_data))
+	callback(their_data, buf, (size_t) len);
+    cbdataUnlock(their_data);
+}
+
+static void
+storeUfsWriteDone(int fd, int errflag, size_t len, void *my_data)
+{
+    storeIOState *sio = my_data;
+    debug(78, 3) ("storeUfsWriteDone: fileno %08X, FD %d, len %d\n",
+	sio->swap_file_number, fd, len);
+    sio->type.ufs.flags.writing = 0;
+    if (errflag) {
+	debug(78, 0) ("storeUfsWriteDone: got failure (%d)\n", errflag);
+	storeUfsIOCallback(sio, errflag);
+	return;
+    }
+    sio->offset += len;
+    if (sio->type.ufs.flags.close_request)
+	storeUfsIOCallback(sio, errflag);
+}
+
+static void
+storeUfsIOCallback(storeIOState * sio, int errflag)
+{
+    debug(78, 3) ("storeUfsIOCallback: errflag=%d\n", errflag);
+    if (sio->type.ufs.fd > -1) {
+	file_close(sio->type.ufs.fd);
+	store_open_disk_fd--;
+    }
+    sio->callback(sio->callback_data, errflag, sio);
+    cbdataFree(sio);
+}
---
+++ tags/SQUID_2_3_DEVEL1/src/store_swapin.c
@@ -0,0 +1,81 @@
+
+/*
+ * $Id$
+ *
+ * DEBUG: section 20    Storage Manager Swapin Functions
+ * AUTHOR: Duane Wessels
+ *
+ * SQUID Internet Object Cache  http://squid.nlanr.net/Squid/
+ * ----------------------------------------------------------
+ *
+ *  Squid is the result of efforts by numerous individuals from the
+ *  Internet community.  Development is led by Duane Wessels of the
+ *  National Laboratory for Applied Network Research and funded by the
+ *  National Science Foundation.  Squid is Copyrighted (C) 1998 by
+ *  Duane Wessels and the University of California San Diego.  Please
+ *  see the COPYRIGHT file for full details.  Squid incorporates
+ *  software developed and/or copyrighted by other sources.  Please see
+ *  the CREDITS file for full details.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+ *
+ */
+
+#include "squid.h"
+
+typedef struct swapin_ctrl_t {
+    StoreEntry *e;
+    SIH *callback;
+    void *callback_data;
+    store_client *sc;
+} swapin_ctrl_t;
+
+static STIOCB storeSwapInFileClosed;
+
+storeIOState *
+storeSwapInStart(StoreEntry * e)
+{
+    storeIOState *sio;
+    assert(e->mem_status == NOT_IN_MEMORY);
+    if (!EBIT_TEST(e->flags, ENTRY_VALIDATED)) {
+	/* We're still reloading and haven't validated this entry yet */
+	return NULL;
+    }
+    debug(20, 3) ("storeSwapInStart: called for %08X %s \n",
+	e->swap_file_number, storeKeyText(e->key));
+    if (e->swap_status != SWAPOUT_WRITING && e->swap_status != SWAPOUT_DONE) {
+	debug(20, 1) ("storeSwapInStart: bad swap_status (%s)\n",
+	    swapStatusStr[e->swap_status]);
+	return NULL;
+    }
+    if (e->swap_file_number < 0) {
+	debug(20, 1) ("storeSwapInStart: swap_file_number < 0\n");
+	return NULL;
+    }
+    assert(e->mem_obj != NULL);
+    debug(20, 3) ("storeSwapInStart: Opening fileno %08X\n",
+	e->swap_file_number);
+    sio = storeOpen(e->swap_file_number, O_RDONLY, storeSwapInFileClosed, NULL);
+    cbdataLock(sio);
+    return sio;
+}
+
+static void
+storeSwapInFileClosed(void *data, int errflag, storeIOState * sio)
+{
+    debug(20, 3) ("storeSwapInFileClosed: sio=%p, errflag=%d\n",
+	sio, errflag);
+    cbdataUnlock(sio);
+}
