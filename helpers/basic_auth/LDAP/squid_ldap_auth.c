239def86b364995c1293dce340466c9f76078d6f
nobody <nobody@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2005-09-22 08:15:26 +0000

    This commit was manufactured by cvs2svn to create tag 'SQUID_2_5_STABLE11'.

--- helpers/basic_auth/LDAP/squid_ldap_auth.c
+++ helpers/basic_auth/LDAP/squid_ldap_auth.c
@@ -259,11 +259,11 @@ open_ldap_connection(const char *ldapServer, int port)
     }
     if (use_tls) {
 #ifdef LDAP_OPT_X_TLS
-        if (version == LDAP_VERSION3 && ldap_start_tls_s(ld, NULL, NULL) != LDAP_SUCCESS) {
-	    fprintf(stderr, "Could not Activate TLS connection\n");
-	    exit(1);
-	} else {
+        if (version != LDAP_VERSION3) {
 	    fprintf(stderr, "TLS requires LDAP version 3\n");
+	    exit(1);
+	} else if (ldap_start_tls_s(ld, NULL, NULL) != LDAP_SUCCESS) {
+	    fprintf(stderr, "Could not Activate TLS connection\n");
 	    exit(1);
 	}
 #else
@@ -672,14 +672,16 @@ checkLDAP(LDAP * persistent_ld, const char *userid, const char *password, const 
     }
 
     if (debug)
-	fprintf(stderr, "attempting to bind to user '%s'\n", dn);
+	fprintf(stderr, "attempting to authenticate user '%s'\n", dn);
     if (!bind_ld && !bind_once)
 	bind_ld = persistent_ld;
     if (!bind_ld)
 	bind_ld = open_ldap_connection(ldapServer, port);
-    if (passwdattr && ldap_compare_s(bind_ld, dn, passwdattr, password) != LDAP_COMPARE_TRUE)
-	ret = 1;
-    else if (ldap_simple_bind_s(bind_ld, dn, password) != LDAP_SUCCESS)
+    if (passwdattr) {
+	if (ldap_compare_s(bind_ld, dn, passwdattr, password) != LDAP_COMPARE_TRUE) {
+	    ret = 1;
+	}
+    } else if (ldap_simple_bind_s(bind_ld, dn, password) != LDAP_SUCCESS)
 	ret = 1;
     if (bind_ld != persistent_ld) {
 	ldap_unbind(bind_ld);
