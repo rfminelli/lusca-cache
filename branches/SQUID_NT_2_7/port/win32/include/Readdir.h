4500c43342c6faf875870b66356d33b50d4ab89b
serassio <serassio@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2008-04-11 23:41:27 +0000

    Windows port: imported Visual Studio project from the nt branch on Sourceforge

---
+++ branches/SQUID_NT_2_7/port/win32/include/Readdir.h
@@ -0,0 +1,33 @@
+/*
+ * Structures and types used to implement opendir/readdir/closedir
+ * on Windows 95/NT.
+*/
+
+#include <io.h>
+#include <stdlib.h>
+#include <sys/types.h> 
+
+#ifdef _MSC_VER /* Microsoft C Compiler ONLY */
+/* struct dirent - same as Unix */
+struct dirent {
+    ino_t d_ino;                    /* inode (always 1 in WIN32) */
+    off_t d_off;                /* offset to this dirent */
+    unsigned short d_reclen;    /* length of d_name */
+    char d_name[_MAX_FNAME+1];    /* filename (null terminated) */
+};
+
+/* typedef DIR - not the same as Unix */
+typedef struct {
+    long handle;                /* _findfirst/_findnext handle */
+    short offset;                /* offset into directory */
+    short finished;             /* 1 if there are not more files */
+    struct _finddata_t fileinfo;  /* from _findfirst/_findnext */
+    char *dir;                  /* the dir we are reading */
+    struct dirent dent;         /* the dirent to return */
+} DIR;
+
+/* Function prototypes */
+DIR * opendir(const char *);
+struct dirent * readdir(DIR *);
+int closedir(DIR *);
+#endif
---
+++ branches/SQUID_NT_2_7/port/win32/squid/buildver.h
@@ -0,0 +1,4 @@
+#define FILEVER        2,5,4,0
+#define PRODUCTVER     2,5,4,0
+#define STRFILEVER     "2, 5, 4, 0\0"
+#define STRPRODUCTVER  "2, 5, 4, 0\0" 
