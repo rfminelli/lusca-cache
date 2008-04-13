a03af243f7d48836aa8d677d49de39a2a35ee86b
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2008-04-13 14:22:46 +0000

    Move the syslog name -> number mapping into a single routine, removing the two duplicate lists (one in the syslog debug code, one in the syslog logging code.)
    Reference: http://devel.squid-cache.org/changesets/squid/patches/10190.patch

--- src/logfile_mod_syslog.c
+++ src/logfile_mod_syslog.c
@@ -36,85 +36,9 @@
 #if HAVE_SYSLOG
 #include "logfile_mod_syslog.h"
 
-/* Define LOG_AUTHPRIV as LOG_AUTH on systems still using the old deprecated LOG_AUTH */
-#if !defined(LOG_AUTHPRIV) && defined(LOG_AUTH)
-#define LOG_AUTHPRIV LOG_AUTH
-#endif
-
-typedef struct {
-    const char *name;
-    int value;
-} syslog_symbol_t;
-
-static int
-syslog_ntoa(const char *s)
-{
-#define syslog_symbol(a) #a, a
-    static syslog_symbol_t symbols[] =
-    {
-#ifdef LOG_AUTHPRIV
-	{syslog_symbol(LOG_AUTHPRIV)},
-#endif
-#ifdef LOG_DAEMON
-	{syslog_symbol(LOG_DAEMON)},
-#endif
-#ifdef LOG_LOCAL0
-	{syslog_symbol(LOG_LOCAL0)},
-#endif
-#ifdef LOG_LOCAL1
-	{syslog_symbol(LOG_LOCAL1)},
-#endif
-#ifdef LOG_LOCAL2
-	{syslog_symbol(LOG_LOCAL2)},
-#endif
-#ifdef LOG_LOCAL3
-	{syslog_symbol(LOG_LOCAL3)},
-#endif
-#ifdef LOG_LOCAL4
-	{syslog_symbol(LOG_LOCAL4)},
-#endif
-#ifdef LOG_LOCAL5
-	{syslog_symbol(LOG_LOCAL5)},
-#endif
-#ifdef LOG_LOCAL6
-	{syslog_symbol(LOG_LOCAL6)},
-#endif
-#ifdef LOG_LOCAL7
-	{syslog_symbol(LOG_LOCAL7)},
-#endif
-#ifdef LOG_USER
-	{syslog_symbol(LOG_USER)},
-#endif
-#ifdef LOG_ERR
-	{syslog_symbol(LOG_ERR)},
-#endif
-#ifdef LOG_WARNING
-	{syslog_symbol(LOG_WARNING)},
-#endif
-#ifdef LOG_NOTICE
-	{syslog_symbol(LOG_NOTICE)},
-#endif
-#ifdef LOG_INFO
-	{syslog_symbol(LOG_INFO)},
-#endif
-#ifdef LOG_DEBUG
-	{syslog_symbol(LOG_DEBUG)},
-#endif
-	{NULL, 0}
-    };
-    syslog_symbol_t *p;
-
-    for (p = symbols; p->name != NULL; ++p)
-	if (!strcmp(s, p->name) || !strcasecmp(s, p->name + 4))
-	    return p->value;
-    return 0;
-}
-
 typedef struct {
     int syslog_priority;
 } l_syslog_t;
-
-#define PRIORITY_MASK (LOG_ERR | LOG_WARNING | LOG_NOTICE | LOG_INFO | LOG_DEBUG)
 
 static void
 logfile_mod_syslog_writeline(Logfile * lf, const char *buf, size_t len)
