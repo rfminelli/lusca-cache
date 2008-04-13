a66699defa45c9d1e94d681ea3bbf406940bbcbe
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2008-04-13 14:22:46 +0000

    Move the syslog name -> number mapping into a single routine, removing the two duplicate lists (one in the syslog debug code, one in the syslog logging code.)
    Reference: http://devel.squid-cache.org/changesets/squid/patches/10190.patch

--- branches/CACHEBOY_PRE/src/debug.c
+++ branches/CACHEBOY_PRE/src/debug.c
@@ -124,119 +124,6 @@ debugOpenLog(const char *logfile)
 }
 
 #if HAVE_SYSLOG
-#ifdef LOG_LOCAL4
-static struct syslog_facility_name {
-    const char *name;
-    int facility;
-} syslog_facility_names[] = {
-
-#ifdef LOG_AUTH
-    {
-	"auth", LOG_AUTH
-    },
-#endif
-#ifdef LOG_AUTHPRIV
-    {
-	"authpriv", LOG_AUTHPRIV
-    },
-#endif
-#ifdef LOG_CRON
-    {
-	"cron", LOG_CRON
-    },
-#endif
-#ifdef LOG_DAEMON
-    {
-	"daemon", LOG_DAEMON
-    },
-#endif
-#ifdef LOG_FTP
-    {
-	"ftp", LOG_FTP
-    },
-#endif
-#ifdef LOG_KERN
-    {
-	"kern", LOG_KERN
-    },
-#endif
-#ifdef LOG_LPR
-    {
-	"lpr", LOG_LPR
-    },
-#endif
-#ifdef LOG_MAIL
-    {
-	"mail", LOG_MAIL
-    },
-#endif
-#ifdef LOG_NEWS
-    {
-	"news", LOG_NEWS
-    },
-#endif
-#ifdef LOG_SYSLOG
-    {
-	"syslog", LOG_SYSLOG
-    },
-#endif
-#ifdef LOG_USER
-    {
-	"user", LOG_USER
-    },
-#endif
-#ifdef LOG_UUCP
-    {
-	"uucp", LOG_UUCP
-    },
-#endif
-#ifdef LOG_LOCAL0
-    {
-	"local0", LOG_LOCAL0
-    },
-#endif
-#ifdef LOG_LOCAL1
-    {
-	"local1", LOG_LOCAL1
-    },
-#endif
-#ifdef LOG_LOCAL2
-    {
-	"local2", LOG_LOCAL2
-    },
-#endif
-#ifdef LOG_LOCAL3
-    {
-	"local3", LOG_LOCAL3
-    },
-#endif
-#ifdef LOG_LOCAL4
-    {
-	"local4", LOG_LOCAL4
-    },
-#endif
-#ifdef LOG_LOCAL5
-    {
-	"local5", LOG_LOCAL5
-    },
-#endif
-#ifdef LOG_LOCAL6
-    {
-	"local6", LOG_LOCAL6
-    },
-#endif
-#ifdef LOG_LOCAL7
-    {
-	"local7", LOG_LOCAL7
-    },
-#endif
-    {
-	NULL, 0
-    }
-};
-
-#endif
-
 void
 _db_set_syslog(const char *facility)
 {
@@ -248,13 +135,10 @@ _db_set_syslog(const char *facility)
     syslog_facility = LOG_LOCAL4;
 #endif
     if (facility) {
-	struct syslog_facility_name *n;
-	for (n = syslog_facility_names; n->name; n++) {
-	    if (strcmp(n->name, facility) == 0) {
-		syslog_facility = n->facility;
-		return;
-	    }
-	}
+        syslog_facility = syslog_ntoa(facility);
+        if (syslog_facility != 0)
+	    return;
+         
 	fprintf(stderr, "unknown syslog facility '%s'\n", facility);
 	exit(1);
     }
