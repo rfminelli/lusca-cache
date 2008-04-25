94394676a1517bfc9fc4a8d6c827029a13c5af32
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2008-04-25 11:49:11 +0000

    Merge changes from trunk
    Revisions: 12577 - 12643

--- branches/CACHEBOY_PRE/src/structs.h
+++ branches/CACHEBOY_PRE/src/structs.h
@@ -457,6 +457,7 @@ struct _SquidConfig {
 	time_t deadPeer;
 	int icp_query;		/* msec */
 	int icp_query_max;	/* msec */
+	int icp_query_min;	/* msec */
 	int mcast_icp_query;	/* msec */
 #if USE_IDENT
 	time_t ident;
@@ -801,6 +802,16 @@ struct _SquidConfig {
     int sleep_after_fork;	/* microseconds */
     time_t minimum_expiry_time;	/* seconds */
     external_acl *externalAclHelperList;
+    enum zph_mode {
+	ZPH_OFF = 0,
+	ZPH_TOS,
+	ZPH_PRIORITY,
+	ZPH_OPTION
+    } zph_mode;
+    int zph_local;
+    int zph_sibling;
+    int zph_parent;
+    int zph_option;
     errormap *errorMapList;
 #if USE_SSL
     struct {
@@ -1298,6 +1309,7 @@ struct _ConnStateData {
 	int auth;		/* pinned for www authentication */
 	peer *peer;		/* peer the connection goes via */
     } pinning;
+    int tos_priority;		/* Used by zph to avoid updating the tos/priority when not needed */
 };
 
 struct _ipcache_addrs {
@@ -1355,6 +1367,8 @@ struct _DigestFetchState {
 	int msg;
 	int bytes;
     } sent, recv;
+    char *buf;
+    size_t buf_used;
 };
 
 /* statistics for cache digests and other hit "predictors" */
