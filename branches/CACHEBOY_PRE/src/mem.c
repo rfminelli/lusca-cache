b279a657a4469a42d62fcddd80e820aecc5af4a0
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2008-05-05 04:06:34 +0000

    Remove MEM_DONTFREE.

--- branches/CACHEBOY_PRE/src/mem.c
+++ branches/CACHEBOY_PRE/src/mem.c
@@ -284,8 +284,6 @@ memCheckInit(void)
 {
     mem_type t;
     for (t = MEM_NONE, t++; t < MEM_MAX; t++) {
-	if (MEM_DONTFREE == t)
-	    continue;
 	/*
 	 * If you hit this assertion, then you forgot to add a
 	 * memDataInit() line for type 't'.
