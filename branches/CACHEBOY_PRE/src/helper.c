3b1f02d58174a4e23b8db0a571b21e723666e7cd
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2008-06-05 03:32:48 +0000

    Fix incorrect cbdata initialiser.

--- branches/CACHEBOY_PRE/src/helper.c
+++ branches/CACHEBOY_PRE/src/helper.c
@@ -589,7 +589,7 @@ helper *
 helperCreate(const char *name)
 {
     helper *hlp;
-    CBDATA_INIT_TYPE(helper_server);
+    CBDATA_INIT_TYPE(helper);
     hlp = cbdataAlloc(helper);
     hlp->id_name = name;
     return hlp;
