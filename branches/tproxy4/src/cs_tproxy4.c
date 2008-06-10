26a81f60079eef8ee3c8adaf79ad6d9fdc981fc6
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2008-06-10 16:47:52 +0000

    This is a mostly copy/paste from Squid-3.HEAD's TPROXY4 implementation. It isn't tested at all and almost certainly won't work; this is mostly just a reminder of how its supposed to work to myself.

--- branches/tproxy4/src/cs_tproxy4.c
+++ branches/tproxy4/src/cs_tproxy4.c
@@ -1,20 +1,40 @@
 
 #include "squid.h"
 
+#ifndef IP_TRANSPARENT
+#error IP_TRANSPARENT must be defined for TPROXY4 support to functioN!
+#endif
+
 int
 cs_bind(int fd, struct in_addr addr, u_short port)
 {
+    int tos = 1;
+    struct sockaddr_in S;
+
+    /* First enable the ability to override the bind restrictions */
+    if (setsockopt(fd, SOL_IP, IP_TRANSPARENT, (char *) &tos, sizeof(int)) < 0) {
+        debug(50, 1) ("comm_open: setsockopt(IP_TRANSPARENT) on FD %d failed: %s\n", fd, xstrerror());
+        return COMM_ERROR;
+    }
+
+    /* Now do the non-local bind */
+    memset(&S, '\0', sizeof(S));
+    S.sin_family = AF_INET;
+    S.sin_port = htons(port);
+    S.sin_addr = addr;
+    statCounter.syscalls.sock.binds++;
+    if (bind(s, (struct sockaddr *) &S, sizeof(S)) == 0)
+        return COMM_OK;
+    debug(50, 1) ("comm_open: setsockopt(IP_TRANSPARENT) on FD %d failed: %s\n", fd, xstrerror());
     return COMM_ERROR;
 }
 
 void
 cs_keepCapabilities(void)
 {
-    need_linux_tproxy = 0;
 }
 
 void
 cs_restoreCapabilities(int keep)
 {
-    need_linux_tproxy = 0;
 }
