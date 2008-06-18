4ac225fe61cdf5d5d0223fc4cac3b5946552c10c
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2008-06-18 15:46:32 +0000

    Add in missing globals!

--- branches/comm_connect_rework/libiapp/globals.c
+++ branches/comm_connect_rework/libiapp/globals.c
@@ -7,3 +7,7 @@ int opt_reuseaddr = 1;
 int iapp_tcpRcvBufSz = 0;
 int iapp_incomingRate;
 const char * iapp_useAcceptFilter = NULL;
+int NHttpSockets = 0;
+int HttpSockets[MAXHTTPPORTS];
+int theInIcpConnection = -1;
+int theOutIcpConnection = -1;
