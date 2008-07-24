2931318e0ebc0b9304009582c7850ccce5c23b1a
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2008-07-24 16:02:29 +0000

    Remove badly laid tag.

--- tags/CACHEBOY_1.4.PRE3/libiapp/mainloop.h
+++
@@ -1,7 +0,0 @@
-#ifndef	__LIBIAPP_MAINLOOP_H__
-#define	__LIBIAPP_MAINLOOP_H__
-
-extern void iapp_init(void);
-extern int iapp_runonce(int msec);
-
-#endif
--- tags/CACHEBOY_1.4.PRE3/libsqinet/inet.h
+++
@@ -1,8 +0,0 @@
-#ifndef	__LIBSQINET_INET_H__
-#define	__LIBSQINET_INET_H__
-
-
-extern const char *xinet_ntoa(const struct in_addr addr);
-
-
-#endif
