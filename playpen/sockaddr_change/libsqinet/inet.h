85e2468e5ff407fca2983289b459158baf65047b
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2008-08-01 07:12:31 +0000

    start fleshing out our local inet type

--- playpen/sockaddr_change/libsqinet/inet.h
+++ playpen/sockaddr_change/libsqinet/inet.h
@@ -1,6 +1,14 @@
 #ifndef	__LIBSQINET_INET_H__
 #define	__LIBSQINET_INET_H__
 
+struct _sqaddr {
+	struct sockaddr_storage st;
+};
+
+typedef struct _sqaddr sqaddr_t;
+
+
+int sqinet_copy_v4_inaddr(sqaddr_t *src, struct struct in_addr *dst, int flags);
 
 extern const char *xinet_ntoa(const struct in_addr addr);
 
