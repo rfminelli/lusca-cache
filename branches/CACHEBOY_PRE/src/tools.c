760a1fe7b2c991e2c03e9e7fe26721276720a1d3
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2008-08-12 08:29:50 +0000

    Remove duplicate copy of kb_incr()

--- branches/CACHEBOY_PRE/src/tools.c
+++ branches/CACHEBOY_PRE/src/tools.c
@@ -891,26 +891,6 @@ dlinkNodeDelete(dlink_node * m)
 }
 
 void
-kb_incr(kb_t * k, squid_off_t v)
-{
-    k->bytes += v;
-    k->kb += (k->bytes >> 10);
-    k->bytes &= 0x3FF;
-    if (k->kb < 0) {
-	/*
-	 * If kb overflows and becomes negative then add powers of
-	 * 2 until it becomes positive again.
-	 */
-	kb_t x;
-	x.kb = 1L << 31;
-	while (x.kb && ((k->kb + x.kb) < 0)) {
-	    x.kb <<= 1;
-	}
-	k->kb += x.kb;
-    }
-}
-
-void
 debugObj(int section, int level, const char *label, void *obj, ObjPackMethod pm)
 {
     MemBuf mb;
