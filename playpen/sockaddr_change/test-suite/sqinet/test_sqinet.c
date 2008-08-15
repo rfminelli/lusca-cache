47224771f0bf7b7c1a0db6e7e8850399a1f91da4
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2008-08-15 11:38:17 +0000

    Add noaddr; add in a few more tests.

--- playpen/sockaddr_change/test-suite/sqinet/test_sqinet.c
+++ playpen/sockaddr_change/test-suite/sqinet/test_sqinet.c
@@ -60,13 +60,51 @@ test1b(void)
 		printf("FAILED: sqinet_is_anyaddr(&s) is false!\n");
 		return 0;
 	}
+	if (sqinet_is_noaddr(&s)) {
+		printf("FAILED: sqinet_is_noaddr(&s) is true when it should be false!\n");
+		return 0;
+	}
 	printf("OK - 0.0.0.0 is anyaddr\n");
 }
+
+static int
+test1c(void)
+{
+	sqaddr_t s;
+	int e;
+	char buf[128];
+	struct in_addr a;
+
+	printf("  sqinet_aton(""255.255.255.255"") parses correctly as INADDR_NONE: ");
+	sqinet_init(&s);
+	e = sqinet_aton(&s, "255.255.255.255", SQATON_FAMILY_IPv4);
+	if (! e) {
+		printf("FAILED: retval != 0\n");
+		return 0;
+	}
+	a = sqinet_get_v4_inaddr(&s, SQADDR_ASSERT_IS_V4);
+	if (a.s_addr != htonl(0xffffffff)) {
+		printf("FAILED: a.s_addr != htonl(0xffffffff) (%X)\n", ntohl(a.s_addr));
+		return 0;
+	}
+
+	if (! sqinet_is_noaddr(&s)) {
+		printf("FAILED: sqinet_is_noaddr(&s) is false!\n");
+		return 0;
+	}
+	if (sqinet_is_anyaddr(&s)) {
+		printf("FAILED: sqinet_is_anyaddr(&s) is true when it should be false!\n");
+		return 0;
+	}
+	printf("OK - 255.255.255.255 is noaddr\n");
+}
+
 
 int
 main(int argc, const char *argv[])
 {
 	printf("Test 1: IPv4 localhost address checks:\n");
-	test1a();	
-	test1b();	
+	test1a();
+	test1b();
+	test1c();
 }
