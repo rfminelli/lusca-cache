e25705462791fb00db8e6b698006cc146cc8fb6d
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2008-08-21 15:37:46 +0000

    * Bring over sqinet.[ch] from the sockaddr_change playpen branch. * Remove the old inet.[ch] files. They probably should've been renamed like they were   in the development branch! * Merge in changes to compile and link it * bring over the handful of test suite code.

--- libiapp/comm.c
+++ libiapp/comm.c
@@ -67,7 +67,7 @@
  
 #include "../libcb/cbdata.h"
 
-#include "../libsqinet/inet.h"
+#include "../libsqinet/sqinet.h"
 #include "../libsqinet/inet_legacy.h"
 
 #include "../libstat/StatHist.h"
--- libsqinet/inet.c
+++
@@ -1,5 +0,0 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
