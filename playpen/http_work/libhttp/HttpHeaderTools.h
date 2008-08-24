1f4c8d5cabd0208472e6068f75bad4b1b03ac71b
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2008-08-24 09:47:52 +0000

    Migrate the httpHeaderFieldInfo* functions to where they belong.

--- playpen/http_work/libhttp/HttpHeaderTools.h
+++ playpen/http_work/libhttp/HttpHeaderTools.h
@@ -2,8 +2,4 @@
 #define	LIBHTTP_HTTPHEADERTOOLS_H__
 
 
-extern HttpHeaderFieldInfo * httpHeaderBuildFieldsInfo(const HttpHeaderFieldAttrs * attrs, int count);
-extern void httpHeaderDestroyFieldsInfo(HttpHeaderFieldInfo * table, int count);
-
-
 #endif
