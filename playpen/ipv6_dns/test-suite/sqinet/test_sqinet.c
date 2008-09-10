66cbc673f0f5f3adaa627ddd8651433453adfe01
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2008-09-10 02:01:26 +0000

    More hacky tests, this time including the revdns output.

--- playpen/ipv6_dns/test-suite/sqinet/test_sqinet.c
+++ playpen/ipv6_dns/test-suite/sqinet/test_sqinet.c
@@ -73,6 +73,8 @@ test1b(void)
 		return 0;
 	}
 	printf("OK - 0.0.0.0 is anyaddr\n");
+	e = sqinet_assemble_rev(&s, buf, 128);
+	printf("  revdns is %s\n", buf);
 }
 
 static int
@@ -113,6 +115,36 @@ test1c(void)
 		return 0;
 	}
 	printf("OK - 255.255.255.255 is noaddr\n");
+	e = sqinet_assemble_rev(&s, buf, 128);
+	printf("  revdns is %s\n", buf);
+}
+
+static int
+test2a(void)
+{
+	sqaddr_t s;
+	int e;
+	char buf[128];
+	struct in_addr a;
+
+	printf("  sqinet_aton(""::0"") parses correctly as INADDR_ANY: ");
+	sqinet_init(&s);
+	e = sqinet_aton(&s, "::0", SQATON_FAMILY_IPv6);
+	if (! e) {
+		printf("FAILED: retval != 0\n");
+		return 0;
+	}
+	if (sqinet_is_noaddr(&s)) {
+		printf("FAILED: sqinet_is_noaddr(&s) is true when it should be false!\n");
+		return 0;
+	}
+	if (! sqinet_is_anyaddr(&s)) {
+		printf("FAILED: sqinet_is_anyaddr(&s) is false when it should be true!\n");
+		return 0;
+	}
+	e = sqinet_assemble_rev(&s, buf, 128);
+	printf("  revdns is %s\n", buf);
+	printf("OK - ::0 is noaddr\n");
 }
 
 
@@ -124,4 +156,5 @@ main(int argc, const char *argv[])
 	test1b();
 	test1c();
 	printf("Test 2: IPv6 address checks:\n");
+	test2a();
 }
