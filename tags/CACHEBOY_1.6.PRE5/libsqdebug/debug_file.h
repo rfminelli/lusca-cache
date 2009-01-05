d7f6ca324011b7549d1c7a8d514d5b92b8dc36bd
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-01-05 18:31:31 +0000

    Tag 1.6.PRE5.

---
+++ tags/CACHEBOY_1.6.PRE5/libsqdebug/debug_file.h
@@ -0,0 +1,14 @@
+#ifndef	__LIBSQDEBUG_DEBUG_H__
+#define	__LIBSQDEBUG_DEBUG_H__
+
+extern FILE *debug_log;         /* NULL */
+extern int opt_debug_rotate_count;
+extern int opt_debug_buffered_logs;
+extern char * opt_debug_log;
+
+extern void _db_print_file(const char *format, va_list args);
+extern void _db_rotate_log(void);
+extern void debugOpenLog(const char *logfile);
+extern void logsFlush(void);
+
+#endif
