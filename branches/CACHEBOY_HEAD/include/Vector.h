88af1687817a4526a33a5703fc00f8ee95f72288
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-01-13 01:31:56 +0000

    inline vector_get() ; since its frequently called and compiles down tiny, just let the compiler evaluate it for you

--- branches/CACHEBOY_HEAD/include/Vector.h
+++ branches/CACHEBOY_HEAD/include/Vector.h
@@ -11,7 +11,8 @@ typedef struct _vector_t vector_t;
 
 void vector_init(vector_t *v, int obj_size, int obj_count);
 void vector_done(vector_t *v);
-void * vector_get(const vector_t *v, int offset);
+void * vector_get_real(const vector_t *v, int offset);
+static inline void * vector_get(const vector_t *v, int offset) { return ((char *) v->data + (v->obj_size * offset)); }
 void * vector_append(vector_t *v);
 void * vector_insert(vector_t *v, int position);
 
--- branches/CACHEBOY_HEAD/lib/Vector.c
+++ branches/CACHEBOY_HEAD/lib/Vector.c
@@ -50,7 +50,7 @@ vector_done(vector_t *v)
 
 
 void *
-vector_get(const vector_t *v, int offset)
+vector_get_real(const vector_t *v, int offset)
 {
 	if (offset > v->used_count)
 		return NULL;
