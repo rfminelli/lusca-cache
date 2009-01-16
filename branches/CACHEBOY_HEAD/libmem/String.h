8be6e08ad1872f5404b392a82abb1a82c408d49b
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-01-16 00:05:57 +0000

    Shuffle around the declarations into areas which need changing vs areas which are fine.
    The "refcounted" string buffer stuff basically requires all the users of
    the "bad" code to be checked and rewritten where required.

--- branches/CACHEBOY_HEAD/libmem/String.h
+++ branches/CACHEBOY_HEAD/libmem/String.h
@@ -11,15 +11,40 @@ struct _String {
 typedef struct _String String;
 
 /* Code using these define's don't treat the buffer as a NUL-terminated C string */
+/* XXX note - the -uses- of these calls don't assume C-string; the String code may not yet! */
 #define strLen2(s)     ((/* const */ int)(s).len)
 #define strBuf2(s)     ((const char*)(s).buf)
+#define strCat(s,str)		stringAppend(&(s), (str), strlen(str))
+#define	strCatStr(ds, ss)	stringAppend(&(ds), strBuf2(ss), strLen(ss))
+
+#define strCmp(s,str)		strcmp(strBuf(s), (str))
+#define strNCmp(s,str,n)	strncmp(strBuf(s), (str), (n))
+#define strCaseCmp(s,str)	strcasecmp(strBuf(s), (str))
+#define strNCaseCmp(s,str,n)	strncasecmp(strBuf(s), (str), (n))
+
+extern void stringInit(String * s, const char *str);
+extern void stringLimitInit(String * s, const char *str, int len);
+extern String stringDup(const String * s);
+extern void stringClean(String * s);
+extern void stringReset(String * s, const char *str);
+extern void stringAppend(String * s, const char *buf, int len);
+extern char * stringDupToC(String *s);
+extern char * stringDupToCOffset(String *s, int offset);
+
+/* These functions need to be re-evaluated and -absolutely- reimplemented or removed */
+
+/* This function doesn't NUL terminate or grow the buffer?! */
+#define strSet(s,ptr,ch) (s).buf[ptr-(s).buf] = (ch)
+/* These is okish */
+#define strCut(s,pos) (((s).len = pos) , ((s).buf[pos] = '\0'))
+/* This function should be removed! */
+#define strCutPtr(s,ptr) (((s).len = (ptr)-(s).buf) , ((s).buf[(s).len] = '\0'))
 
 /*
  * These two functions return whether the string is set to some value, even if
  * its an empty string. A few Squid functions do if (strBuf(str)) to see if
  * something has set the string to a value; these functions replace them.
  */
-
 #define	strIsNull(s)	( (s).buf == NULL )
 #define	strIsNotNull(s)	( (s).buf != NULL )
 
@@ -30,26 +55,10 @@ typedef struct _String String;
 #define strChr(s,ch)  ((const char*)strchr(strBuf(s), (ch)))
 #define strRChr(s,ch) ((const char*)strrchr(strBuf(s), (ch)))
 #define strStr(s,str) ((const char*)strstr(strBuf(s), (str)))  
-#define strCmp(s,str)     strcmp(strBuf(s), (str))
-#define strNCmp(s,str,n)     strncmp(strBuf(s), (str), (n))
-#define strCaseCmp(s,str) strcasecmp(strBuf(s), (str))
-#define strNCaseCmp(s,str,n) strncasecmp(strBuf(s), (str), (n))
-#define strSet(s,ptr,ch) (s).buf[ptr-(s).buf] = (ch)
-#define strCut(s,pos) (((s).len = pos) , ((s).buf[pos] = '\0'))
-#define strCutPtr(s,ptr) (((s).len = (ptr)-(s).buf) , ((s).buf[(s).len] = '\0'))
-#define strCat(s,str)  stringAppend(&(s), (str), strlen(str))
-#define	strCatStr(ds, ss)	stringAppend(&(ds), strBuf2(ss), strLen(ss))
 
-extern void stringInit(String * s, const char *str);
-extern void stringLimitInit(String * s, const char *str, int len);
-extern String stringDup(const String * s);
-extern void stringClean(String * s);
-extern void stringReset(String * s, const char *str);
-extern void stringAppend(String * s, const char *buf, int len);
+
 /* extern void stringAppendf(String *s, const char *fmt, ...) PRINTF_FORMAT_ARG2; */
 
-extern char * stringDupToC(String *s);
-extern char * stringDupToCOffset(String *s, int offset);
 
 extern const String StringNull; /* { 0, 0, NULL } */
 
