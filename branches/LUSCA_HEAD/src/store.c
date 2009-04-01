bf8b52d5aef000f8783c64aa839697e5fae79d05
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-04-01 22:41:36 +0000

    Fix compile time type conversion warnings.

--- branches/LUSCA_HEAD/src/store.c
+++ branches/LUSCA_HEAD/src/store.c
@@ -615,6 +615,7 @@ strcmpnull(const char *a, const char *b)
     return 0;
 }
 
+#if 0
 static int
 strncmpnull(const char *a, const char *b, size_t n)
 {
@@ -626,6 +627,7 @@ strncmpnull(const char *a, const char *b, size_t n)
 	return -1;
     return 0;
 }
+#endif
 
 static void
 storeAddVaryReadOld(void *data, mem_node_ref nr, ssize_t size)
@@ -1718,7 +1720,7 @@ storeInitHashValues(void)
     /* Calculate size of hash table (maximum currently 64k buckets).  */
     i = (Config.Swap.maxSize + (Config.memMaxSize >> 10)) / Config.Store.avgObjectSize;
     debug(20, 1) ("Swap maxSize %lu + %lu KB, estimated %ld objects\n",
-	(unsigned long int) Config.Swap.maxSize, (Config.memMaxSize >> 10), i);
+	(unsigned long int) Config.Swap.maxSize, (long int) (Config.memMaxSize >> 10), (unsigned long int) i);
     i /= Config.Store.objectsPerBucket;
     debug(20, 1) ("Target number of buckets: %ld\n", i);
     /* ideally the full scan period should be configurable, for the
