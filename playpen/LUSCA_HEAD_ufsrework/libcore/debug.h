552998b8c01672622f62a6c945982cdd66d8a838
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-04-13 13:01:57 +0000

    There's no need for this branch as it stands - the diskd deletion has happened; some other reorg will happen in HEAD before the branch for intrusive changes will happen.

--- playpen/LUSCA_HEAD_ufsrework/libcore/debug.h
+++
@@ -1,5 +0,0 @@
-#ifndef	__LIBCORE_DEBUG_H__
-
-#include "../libsqdebug/debug.h"
-
-#endif
--- playpen/LUSCA_HEAD_ufsrework/libhttp/HttpHeaderTools.h
+++
@@ -1,6 +0,0 @@
-#ifndef	LIBHTTP_HTTPHEADERTOOLS_H__
-#define	LIBHTTP_HTTPHEADERTOOLS_H__
-
-extern int httpHeaderHasConnDir(const HttpHeader * hdr, const char *directive);
-
-#endif
