372b8fd9aeb9379f98c609278dd67b5e032e06c0
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-04-20 07:29:38 +0000

    start fleshing out the "shared" UFS style store routines from ufs/aufs/coss (which will hopefully eventually be unified much more than they are now.)

---
+++ branches/LUSCA_HEAD/src/ufs_utils.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "../libcore/varargs.h"
+#include "../libsqdebug/debug.h"
+
---
+++ branches/LUSCA_HEAD/src/ufs_utils.h
@@ -0,0 +1,6 @@
+#ifndef	__LUSCA_UFS_UTILS_H__
+#define	__LUSCA_UFS_UTILS_H__
+
+
+
+#endif
