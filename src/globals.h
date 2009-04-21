3f209d99c3d2ea83af9c7c4ddd3a44d078b2f713
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-04-21 08:55:28 +0000

    Link libsqstore into the build; break out some of the basic store typedefs/enums.

--- src/globals.h
+++ src/globals.h
@@ -95,7 +95,6 @@ extern time_t hit_only_mode_until;	/* 0 */
 extern StatCounters statCounter;
 extern const char *err_type_str[];
 extern const char *icp_opcode_str[];
-extern const char *swap_log_op_str[];
 extern const char *lookup_t_str[];
 extern double request_failure_ratio;	/* 0.0 */
 extern int store_hash_buckets;	/* 0 */
