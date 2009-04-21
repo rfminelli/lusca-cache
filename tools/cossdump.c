a2d8d0fef4f4be9da696b1eef3cbf0a039b444ec
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-04-21 09:26:23 +0000

    Hack the cossdump program to start using the reusable code rather than copy/pasted stuff of its own.

--- tools/cossdump.c
+++ tools/cossdump.c
@@ -12,9 +12,6 @@
 #if HAVE_UNISTD_H
 #include <unistd.h>
 #endif
-#if HAVE_ASSERT_H
-#include <assert.h>
-#endif
 #if HAVE_STRING_H
 #include <string.h>
 #endif
@@ -26,32 +23,18 @@
 #endif
 
 #include "../libcore/kb.h"
+#include "../libcore/varargs.h"
+
+#include "../libsqdebug/debug.h"
+
+#include "../libsqtlv/tlv.h"
+
 #include "../libsqstore/store_mgr.h"
 
 #include "../src/defines.h"
 /* XXX Needed for stuff in enums.h; remove later! -adrian */
 #include "../libhttp/HttpHeaderType.h"
 #include "../src/enums.h"
-
-
-struct _tlv;
-typedef struct _tlv tlv;
-
-struct _tlv {
-    char type;
-    int length;
-    void *value;
-    tlv *next;
-};
-
-#undef debug
-#define	debug(a, b)	printf
-
-#define	MEM_TLV	sizeof(tlv)
-#define	memAllocate(a)	malloc(a)
-#define	memFree(a, b)	free(a)
-#define xmalloc(a) malloc(a)
-#define xfree(a) free(a)
 
 #ifndef PRId64
 #ifdef _SQUID_MSWIN_		/* Windows native port using MSVCRT */
@@ -73,114 +56,13 @@ struct _tlv {
 #define strto_off_t strtol
 #endif
 
-static tlv **
-storeSwapTLVAdd(int type, const void *ptr, size_t len, tlv ** tail)
-{
-    tlv *t = memAllocate(MEM_TLV);
-    t->type = (char) type;
-    t->length = (int) len;
-    t->value = xmalloc(len);
-    xmemcpy(t->value, ptr, len);
-    *tail = t;
-    return &t->next;		/* return new tail pointer */
-}
-
-#if UNUSED_CODE
-static void
-storeSwapTLVFree(tlv * n)
-{
-    tlv *t;
-    while ((t = n) != NULL) {
-	n = t->next;
-	xfree(t->value);
-	memFree(t, MEM_TLV);
-    }
-}
-#endif
-
-#if UNUSED_CODE
-static char *
-storeSwapMetaPack(tlv * tlv_list, int *length)
-{
-    int buflen = 0;
-    tlv *t;
-    int j = 0;
-    char *buf;
-    assert(length != NULL);
-    buflen++;			/* STORE_META_OK */
-    buflen += sizeof(int);	/* size of header to follow */
-    for (t = tlv_list; t; t = t->next)
-	buflen += sizeof(char) + sizeof(int) + t->length;
-    buflen++;			/* STORE_META_END */
-    buf = xmalloc(buflen);
-    buf[j++] = (char) STORE_META_OK;
-    xmemcpy(&buf[j], &buflen, sizeof(int));
-    j += sizeof(int);
-    for (t = tlv_list; t; t = t->next) {
-	buf[j++] = (char) t->type;
-	xmemcpy(&buf[j], &t->length, sizeof(int));
-	j += sizeof(int);
-	xmemcpy(&buf[j], t->value, t->length);
-	j += t->length;
-    }
-    buf[j++] = (char) STORE_META_END;
-    assert((int) j == buflen);
-    *length = buflen;
-    return buf;
-}
-#endif
-
-static tlv *
-storeSwapMetaUnpack(const char *buf, int *hdr_len)
-{
-    tlv *TLV;			/* we'll return this */
-    tlv **T = &TLV;
-    char type;
-    int length;
-    int buflen;
-    int j = 0;
-    assert(buf != NULL);
-    assert(hdr_len != NULL);
-    if (buf[j++] != (char) STORE_META_OK)
-	return NULL;
-    xmemcpy(&buflen, &buf[j], sizeof(int));
-    j += sizeof(int);
-    /*
-     * sanity check on 'buflen' value.  It should be at least big
-     * enough to hold one type and one length.
-     */
-    if (buflen <= (sizeof(char) + sizeof(int)))
-	    return NULL;
-    while (buflen - j > (sizeof(char) + sizeof(int))) {
-	type = buf[j++];
-	/* VOID is reserved, but allow some slack for new types.. */
-	if (type <= STORE_META_VOID || type > STORE_META_END + 10) {
-	    debug(20, 0) ("storeSwapMetaUnpack: bad type (%d)!\n", type);
-	    break;
-	}
-	xmemcpy(&length, &buf[j], sizeof(int));
-	if (length < 0 || length > (1 << 16)) {
-	    debug(20, 0) ("storeSwapMetaUnpack: insane length (%d)!\n", length);
-	    break;
-	}
-	j += sizeof(int);
-	if (j + length > buflen) {
-	    debug(20, 0) ("storeSwapMetaUnpack: overflow!\n");
-	    debug(20, 0) ("\ttype=%d, length=%d, buflen=%d, offset=%d\n",
-		type, length, buflen, (int) j);
-	    break;
-	}
-	T = storeSwapTLVAdd(type, &buf[j], (size_t) length, T);
-	j += length;
-    }
-    *hdr_len = buflen;
-    return TLV;
-}
-
-
 #define	STRIPESIZE 1048576
 #define	BLOCKSIZE 1024
 #define BLKBITS 10
+
+
+/* normally in libiapp .. */
+int shutting_down = 0;
 
 static void
 parse_stripe(int stripeid, char *buf, int len, int blocksize)
@@ -194,11 +76,13 @@ parse_stripe(int stripeid, char *buf, int len, int blocksize)
     while (j < len) {
 	l = NULL;
 	bl = 0;
-	tlv_list = storeSwapMetaUnpack(&buf[j], &bl);
+	tlv_list = tlv_unpack(&buf[j], &bl, STORE_META_END + 10);
 	if (tlv_list == NULL) {
 	    printf("  Object: NULL\n");
 	    return;
 	}
+	/* XXX need to make sure the first entry in the list is type STORE_META_OK ? (an "int" type) */
+
 	printf("  Object: (filen %d) hdr size %d\n", j / blocksize + (stripeid * STRIPESIZE / blocksize), bl);
 	for (t = tlv_list; t; t = t->next) {
 	    switch (t->type) {
@@ -221,6 +105,8 @@ parse_stripe(int stripeid, char *buf, int len, int blocksize)
 	tmp = j / blocksize;
 	tmp = (tmp + 1) * blocksize;
 	j = tmp;
+
+	tlv_free(tlv_list);
     }
 }
 
@@ -233,6 +119,10 @@ main(int argc, char *argv[])
     unsigned int numstripes = 0;
     int blocksize = BLOCKSIZE;
     int blksize_bits;
+
+    /* Setup the debugging library */
+    _db_init("ALL,1");
+    _db_set_stderr_debug(1);
 
     if (argc < 4) {
 	printf("Usage: %s <path to COSS datafile> <blocksize> <number of stripes>\n", argv[0]);
