513215850bada4177eb2b688a7e1f4dac9382b26
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-04-21 09:56:02 +0000

    placeholder to put some swap metadata info.

---
+++ branches/LUSCA_HEAD/libsqstore/store_meta.h
@@ -0,0 +1,11 @@
+#ifndef	__LIBSQSTORE_STORE_META_H__
+#define	__LIBSQSTORE_STORE_META_H__
+
+/*
+ * For now these aren't used in the application itself; they're
+ * designed to be used by other bits of code which are manipulating
+ * store swap entries.
+ */
+
+
+#endif
