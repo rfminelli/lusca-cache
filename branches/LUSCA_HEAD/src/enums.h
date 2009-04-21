d1d11802f1212f5908f0c331fbd8d243c64005f4
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-04-21 09:11:04 +0000

    Migrate out the store meta types into libsqstore; convert cossdump.c to use these.

--- branches/LUSCA_HEAD/src/enums.h
+++ branches/LUSCA_HEAD/src/enums.h
@@ -375,25 +375,6 @@ enum {
 
 #endif
 
-/*
- * NOTE!  We must preserve the order of this list!
- */
-enum {
-    STORE_META_VOID,		/* should not come up */
-    STORE_META_KEY_URL,		/* key w/ keytype */
-    STORE_META_KEY_SHA,
-    STORE_META_KEY_MD5,
-    STORE_META_URL,		/* the url , if not in the header */
-    STORE_META_STD,		/* standard metadata */
-    STORE_META_HITMETERING,	/* reserved for hit metering */
-    STORE_META_VALID,
-    STORE_META_VARY_HEADERS,	/* Stores Vary request headers */
-    STORE_META_STD_LFS,		/* standard metadata in lfs format */
-    STORE_META_OBJSIZE,		/* object size, if its known */
-    STORE_META_STOREURL,	/* the store url, if different to the normal URL */
-    STORE_META_END
-};
-
 enum {
     STORE_LOG_CREATE,
     STORE_LOG_SWAPIN,
