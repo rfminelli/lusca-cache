26d23c13f6308e8a936c46b52421a1ed3d9cd2c2
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-04-26 16:27:31 +0000

    Create store_file_ufs.c; start migrating UFS specific magic into here.

--- branches/LUSCA_HEAD/libsqstore/store_log.c
+++ branches/LUSCA_HEAD/libsqstore/store_log.c
@@ -8,7 +8,6 @@
 
 #include "../libcore/varargs.h"
 #include "../libcore/kb.h"
-#include "../libcore/tools.h"	/* for SQUID_MAXPATHLEN */
 
 #include "store_mgr.h"
 #include "store_log.h"
@@ -20,32 +19,3 @@ const char * swap_log_op_str[] = {
     "SWAP_LOG_VERSION",
     "SWAP_LOG_MAX"
 };
-
-/*
- * This shouldn't be here for now.. :)
- */
-
-/*
- * Create a UFS path given the component bits.
- *
- * "buf" must be SQUID_MAXPATHLEN.
- */
-int
-store_ufs_createPath(const char *prefix, int filn, int L1, int L2, char *buf)
-{   
-    buf[0] = '\0';
-    snprintf(buf, SQUID_MAXPATHLEN, "%s/%02X/%02X/%08X",
-        prefix,
-        ((filn / L2) / L2) % L1,
-        (filn / L2) % L2,
-        filn);
-    return 1;
-}   
-
-int
-store_ufs_createDir(const char *prefix, int L1, int L2, char *buf)
-{
-    buf[0] = '\0';
-    snprintf(buf, SQUID_MAXPATHLEN, "%s/%02X/%02X", prefix, L1, L2);
-    return 1;
-}
