a83d86e776c7f676571aa95ee1af9d8787d20cd9
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-04-26 14:27:15 +0000

    Start fleshing out some UFS store file routines; in the "wrong" place for now. (I'll move them to a separate file later.)

---
+++ branches/LUSCA_HEAD/libsqstore/store_file_ufs.h
@@ -0,0 +1,6 @@
+#ifndef	__LIBSQSTORE_STORE_FILE_UFS_H__
+#define	__LIBSQSTORE_STORE_FILE_UFS_H__
+
+extern int store_ufs_createPath(const char *prefix, int swap_filen, int L1, int L2, char *buf);
+
+#endif
--- branches/LUSCA_HEAD/libsqstore/store_log.c
+++ branches/LUSCA_HEAD/libsqstore/store_log.c
@@ -7,6 +7,7 @@
 #include "../include/squid_md5.h"
 
 #include "../libcore/kb.h"
+#include "../libcore/tools.h"	/* for SQUID_MAXPATHLEN */
 
 #include "store_mgr.h"
 #include "store_log.h"
@@ -19,3 +20,23 @@ const char * swap_log_op_str[] = {
     "SWAP_LOG_MAX"
 };
 
+/*
+ * This shouldn't be here for now.. :)
+ */
+
+/*
+ * Create a UFS path given the component bits.
+ *
+ * "buf" must be SQUID_MAXPATHLEN.
+ */
+int
+store_ufs_createPath(const char *prefix, int filn, int L1, int L2, char *buf)
+{   
+    buf[0] = '\0';
+    snprintf(buf, SQUID_MAXPATHLEN, "%s/%02X/%02X/%08X",
+        prefix,
+        ((filn / L2) / L2) % L1,
+        (filn / L2) % L2,
+        filn);
+    return 1;
+}   
