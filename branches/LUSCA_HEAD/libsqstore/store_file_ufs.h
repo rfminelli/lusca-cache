91faf845b4eaa6b22112ec37b760e95498e6dac6
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-04-28 11:24:35 +0000

    Fix bad conversaion

--- branches/LUSCA_HEAD/libsqstore/store_file_ufs.h
+++ branches/LUSCA_HEAD/libsqstore/store_file_ufs.h
@@ -12,7 +12,7 @@ extern void store_ufs_init(store_ufs_dir_t *sd, const char *path, int l1, int l2
 extern void store_ufs_done(store_ufs_dir_t *sd);
 
 extern int store_ufs_createPath(store_ufs_dir_t *sd, int swap_filen, char *buf);
-extern int store_ufs_createDir(store_ufs_dir_t *sd, char *buf);
+extern int store_ufs_createDir(store_ufs_dir_t *sd, int d1, int d2, char *buf);
 extern int store_ufs_filenum_correct_dir(store_ufs_dir_t *sd, int fn, int F1, int F2);
 
 static inline int store_ufs_l1(store_ufs_dir_t *sd) { return sd->l1; }
