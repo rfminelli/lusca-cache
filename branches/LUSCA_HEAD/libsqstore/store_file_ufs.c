ebc6d3f09cf99d4e05db4be7342089da77ae453c
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-04-28 07:35:54 +0000

    Break out another UFS/AUFS shared routine - one which checks the given UFS file belongs in the given directory.

--- branches/LUSCA_HEAD/libsqstore/store_file_ufs.c
+++ branches/LUSCA_HEAD/libsqstore/store_file_ufs.c
@@ -42,3 +42,26 @@ store_ufs_createDir(const char *prefix, int L1, int L2, char *buf)
     snprintf(buf, SQUID_MAXPATHLEN, "%s/%02X/%02X", prefix, L1, L2);
     return 1;
 }
+
+/*
+ * F1/F2 - current directory numbers which they're in
+ * L1/L2 - configured storedir L1/L2
+ * fn - file number
+ *
+ * returns whether "fn" belongs in the directory F1/F2 given the configured L1/L2
+ */
+int
+store_ufs_filenum_correct_dir(int fn, int F1, int F2, int L1, int L2)
+{
+    int D1, D2;
+    int filn = fn;
+
+    D1 = ((filn / L2) / L2) % L1;
+    if (F1 != D1)
+        return 0;
+    D2 = (filn / L2) % L2;
+    if (F2 != D2)
+        return 0;
+    return 1;
+}
+
