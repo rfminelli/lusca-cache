13801f285ddc98ebb8a6f460b1c0c02898655d47
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-04-28 07:35:54 +0000

    Break out another UFS/AUFS shared routine - one which checks the given UFS file belongs in the given directory.

--- libsqstore/store_file_ufs.h
+++ libsqstore/store_file_ufs.h
@@ -3,5 +3,7 @@
 
 extern int store_ufs_createPath(const char *prefix, int swap_filen, int L1, int L2, char *buf);
 extern int store_ufs_createDir(const char *prefix, int L1, int L2, char *buf);
+extern int store_ufs_filenum_correct_dir(int fn, int F1, int F2, int L1, int L2);
+
 
 #endif
