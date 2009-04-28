15da0a3c3910a15fa3ad0a5b72d1931b2101d4d2
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-04-28 11:23:25 +0000

    Migrate the store ufs functions to use the new class instance info

--- libsqstore/store_file_ufs.h
+++ libsqstore/store_file_ufs.h
@@ -11,9 +11,12 @@ typedef struct _store_ufs_dir store_ufs_dir_t;
 extern void store_ufs_init(store_ufs_dir_t *sd, const char *path, int l1, int l2);
 extern void store_ufs_done(store_ufs_dir_t *sd);
 
-extern int store_ufs_createPath(const char *prefix, int swap_filen, int L1, int L2, char *buf);
-extern int store_ufs_createDir(const char *prefix, int L1, int L2, char *buf);
-extern int store_ufs_filenum_correct_dir(int fn, int F1, int F2, int L1, int L2);
+extern int store_ufs_createPath(store_ufs_dir_t *sd, int swap_filen, char *buf);
+extern int store_ufs_createDir(store_ufs_dir_t *sd, char *buf);
+extern int store_ufs_filenum_correct_dir(store_ufs_dir_t *sd, int fn, int F1, int F2);
 
+static inline int store_ufs_l1(store_ufs_dir_t *sd) { return sd->l1; }
+static inline int store_ufs_l2(store_ufs_dir_t *sd) { return sd->l2; }
+static inline const char * store_ufs_path(store_ufs_dir_t *sd) { return sd->path; }
 
 #endif
