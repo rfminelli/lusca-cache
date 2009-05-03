2d17b9944003a63b80670644fcaee865295b7a8b
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-05-03 03:41:47 +0000

    Migrate ufsdump to use the store log API header printing code; update the code to use the file number in the log printing.

--- branches/LUSCA_HEAD/tools/ufsdump.c
+++ branches/LUSCA_HEAD/tools/ufsdump.c
@@ -55,6 +55,7 @@ struct _rebuild_entry {
 	char *storeurl;
 	squid_file_sz file_size;		/* swap file size - object size + metadata */
 	int hdr_size;				/* metadata size */
+	int swap_filen;
 };
 typedef struct _rebuild_entry rebuild_entry_t;
 
@@ -72,6 +73,7 @@ rebuild_entry_init(rebuild_entry_t *re)
 	bzero(re, sizeof(*re));
 	re->hdr_size = -1;
 	re->file_size = -1;
+	re->swap_filen = -1;
 }
 
 const char *
@@ -177,7 +179,7 @@ write_swaplog_entry(rebuild_entry_t *re)
 	storeSwapLogData sd;
 
 	sd.op = SWAP_LOG_ADD;
-	sd.swap_filen = 0x12345678;		/* XXX this should be based on the filename */
+	sd.swap_filen = re->swap_filen;
 	sd.timestamp = re->mi.timestamp;
 	sd.lastref = re->mi.lastref;
 	sd.expires = re->mi.expires;
@@ -238,6 +240,7 @@ read_dir(store_ufs_dir_t *sd)
 
 				rebuild_entry_init(&re);
 				(void) read_file(path, &re);
+				re.swap_filen = fn;
 				(void) write_swaplog_entry(&re);
 				rebuild_entry_done(&re);
 
@@ -245,19 +248,6 @@ read_dir(store_ufs_dir_t *sd)
 			closedir(d);
 		}
 	}
-}
-
-void
-print_header(store_ufs_dir_t *ud, int fd)
-{
-    char buf[sizeof(storeSwapLogData)];
-    storeSwapLogHeader *sh = (storeSwapLogHeader *) buf;
-
-    bzero(buf, sizeof(buf));
-    sh->op = SWAP_LOG_VERSION;
-    sh->version = 1;
-    sh->record_size = sizeof(storeSwapLogData);
-    write(1, sh, sizeof(storeSwapLogData));
 }
 
 int
@@ -276,8 +266,8 @@ main(int argc, char *argv[])
 
     store_ufs_init(&store_ufs_info, argv[1], atoi(argv[2]), atoi(argv[3]));
 
-    /* Output swap header */
-    print_header(&store_ufs_info, 1);
+    /* Output swap header to stdout */
+    (void) storeSwapLogPrintHeader(1);
 
     read_dir(&store_ufs_info);
     store_ufs_done(&store_ufs_info);
