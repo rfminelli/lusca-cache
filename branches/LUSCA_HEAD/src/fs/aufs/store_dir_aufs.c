b5b18c9b8b7f9e003236ea9825207c6382b57c9a
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-05-03 23:06:23 +0000

    Break out the rebuild state stuff.

--- branches/LUSCA_HEAD/src/fs/aufs/store_dir_aufs.c
+++ branches/LUSCA_HEAD/src/fs/aufs/store_dir_aufs.c
@@ -38,33 +38,11 @@
 #include "../../libasyncio/aiops.h"
 #include "../../libasyncio/async_io.h"
 #include "store_asyncufs.h"
+#include "store_rebuild_aufs.h"
 
 #define DefaultLevelOneDirs     16
 #define DefaultLevelTwoDirs     256
 #define STORE_META_BUFSZ 4096
-
-typedef struct _RebuildState RebuildState;
-struct _RebuildState {
-    SwapDir *sd;
-    int n_read;
-    int log_fd;
-    int speed;
-    int curlvl1;
-    int curlvl2;
-    struct {
-	unsigned int clean:1;
-	unsigned int init:1;
-	unsigned int old_swaplog_entry_size:1;
-    } flags;
-    int done;
-    int in_dir;
-    int fn;
-    struct dirent *entry;
-    DIR *td;
-    char fullpath[SQUID_MAXPATHLEN];
-    char fullfilename[SQUID_MAXPATHLEN];
-    struct _store_rebuild_data counts;
-};
 
 int n_asyncufs_dirs = 0;
 static int *asyncufs_dir_index = NULL;
