4a22584bafd632681ee76a923bcb8ae08319b296
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-07-10 05:57:32 +0000

    Remove the old unused rebuild code.

--- playpen/LUSCA_HEAD_storework/src/fs/aufs/store_rebuild_aufs.h
+++ playpen/LUSCA_HEAD_storework/src/fs/aufs/store_rebuild_aufs.h
@@ -6,23 +6,11 @@ struct _RebuildState {
     SwapDir *sd;
     int n_read;
     int log_fd;
-    int speed;
-    int curlvl1;
-    int curlvl2;
     struct {
         unsigned int clean:1;
         unsigned int init:1;
-        unsigned int old_swaplog_entry_size:1;
     } flags;
-    int done;
-    int in_dir;
-    int fn;
-    struct dirent *entry;
-    DIR *td;
-    char fullpath[SQUID_MAXPATHLEN];
-    char fullfilename[SQUID_MAXPATHLEN];
     struct _store_rebuild_data counts;
-
     struct {
 	int r_fd, w_fd;
 	pid_t pid;
