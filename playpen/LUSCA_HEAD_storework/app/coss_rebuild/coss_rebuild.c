4ab32a3a4eeceb3e0c866ec90a822a578bc663c2
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-07-17 03:45:11 +0000

    Introduce a "cmd" option for later.

--- playpen/LUSCA_HEAD_storework/app/coss_rebuild/coss_rebuild.c
+++ playpen/LUSCA_HEAD_storework/app/coss_rebuild/coss_rebuild.c
@@ -14,6 +14,7 @@ int shutting_down = 0;	/* needed for libiapp */
 int
 main(int argc, const char *argv[])
 {
+	const char *cmd;
 	const char *path;
 	int block_size;
 	size_t stripe_size;
@@ -23,10 +24,11 @@ main(int argc, const char *argv[])
         _db_init("ALL,1");
         _db_set_stderr_debug(1);
 
-	path = argv[1];
-	block_size = atoi(argv[2]);
-	stripe_size = atoi(argv[3]);
-	num_stripes = atoi(argv[4]);
+	cmd = argv[1];
+	path = argv[2];
+	block_size = atoi(argv[3]);
+	stripe_size = atoi(argv[4]);
+	num_stripes = atoi(argv[5]);
 
 	(void) coss_rebuild_dir(path, stripe_size, block_size, num_stripes);
 
