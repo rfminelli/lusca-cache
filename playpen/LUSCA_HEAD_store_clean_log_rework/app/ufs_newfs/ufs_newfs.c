428cae5c4c2e8dada2e77861256ea521f7b78c39
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-07-23 00:49:50 +0000

    Create a branch for the swaplog clean writing work

---
+++ playpen/LUSCA_HEAD_store_clean_log_rework/app/ufs_newfs/ufs_newfs.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include "include/config.h"
+#include "include/util.h"
+
+#include "libcore/tools.h"
+#include "libcore/varargs.h"
+#include "libsqdebug/debug.h"
+#include "libsqstore/store_file_ufs.h"
+
+int shutting_down = 0;	/* needed for debug routines for now */
+
+int  
+storeAufsDirCreateDirectory(const char *path, int should_exist)
+{  
+    int created = 0;
+    struct stat st;
+    getCurrentTime();
+    if (0 == stat(path, &st)) {
+        if (S_ISDIR(st.st_mode)) {
+            debug(47, should_exist ? 3 : 1) ("%s exists\n", path);
+        } else {
+            libcore_fatalf("Swap directory %s is not a directory.\n", path);
+        }
+#ifdef _SQUID_MSWIN_
+    } else if (0 == mkdir(path)) {
+#else
+    } else if (0 == mkdir(path, 0755)) {
+#endif
+        debug(47, should_exist ? 1 : 3) ("%s created\n", path);
+        created = 1;
+    } else {
+        libcore_fatalf("Failed to make swap directory %s: %s\n", path, xstrerror());
+    }
+    return created;
+}
+
+void
+storeAufsDirCreateSwapSubDirs(store_ufs_dir_t *sd)
+{   
+    int i, k;
+    int should_exist;
+    char name[MAXPATHLEN];
+
+    for (i = 0; i < sd->l1; i++) {
+        snprintf(name, MAXPATHLEN, "%s/%02X", sd->path, i);
+        if (storeAufsDirCreateDirectory(name, 0))
+            should_exist = 0;
+        else
+            should_exist = 1;
+        debug(47, 1) ("Making directories in %s\n", name);
+        for (k = 0; k < sd->l2; k++) {
+            snprintf(name, MAXPATHLEN, "%s/%02X/%02X", sd->path, i, k);
+            storeAufsDirCreateDirectory(name, should_exist);
+        }
+    }
+}
+
+int
+main(int argc, const char *argv[])
+{
+	store_ufs_dir_t sd;
+
+	store_ufs_init(&sd, argv[1], atoi(argv[2]), atoi(argv[3]), "/tmp/f");
+	storeAufsDirCreateSwapSubDirs(&sd);
+	store_ufs_done(&sd);
+
+	exit(0);
+}
---
+++ playpen/LUSCA_HEAD_store_clean_log_rework/app/ufs_rebuild/ufs_rebuild.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "include/config.h"
+#include "include/squid_md5.h"
+
+#include "libcore/varargs.h"
+#include "libcore/kb.h"
+#include "libsqdebug/debug.h"
+#include "libsqstore/store_mgr.h"
+#include "libsqstore/store_log.h"
+#include "libsqstore/store_file_ufs.h"
+
+#include "ufs_build_dir.h"
+#include "ufs_build_log.h"
+
+#define	WRITE_BUFFER_LEN	65536
+
+int shutting_down = 0;
+
+typedef enum {
+	REBUILD_NONE,
+	REBUILD_DISK,
+	REBUILD_LOG
+} rebuild_type_t;
+
+static rebuild_type_t
+probe_dir(store_ufs_dir_t *u)
+{
+	if (store_ufs_has_valid_rebuild_log(u))
+		return REBUILD_LOG;
+	return REBUILD_DISK;
+}
+
+static void
+usage(const char *cmdname)
+{
+	printf("Usage: %s <command> <store path> <l1> <l2> <path to swapfile>\n", cmdname);
+	printf("  where <command> is one of rebuild-dir, rebuild-log or rebuild.\n");
+}
+
+
+int
+main(int argc, char *argv[])
+{
+	const char *cmd;
+	store_ufs_dir_t store_ufs_info;
+	rebuild_type_t rebuild_type;
+	char *wbuf = NULL;
+
+	if (argc < 5) {
+		usage(argv[0]);
+		exit(1);
+	}
+	cmd = argv[1];
+
+	wbuf = malloc(WRITE_BUFFER_LEN);
+	if (wbuf) {
+		setbuffer(stdout, wbuf, WRITE_BUFFER_LEN);
+	}
+
+	/* Setup the debugging library */
+	_db_init("ALL,1");
+	_db_set_stderr_debug(1);
+
+
+	store_ufs_init(&store_ufs_info, argv[2], atoi(argv[3]), atoi(argv[4]), argv[5]);
+
+	if (strcmp(cmd, "rebuild-dir") == 0) {
+		rebuild_type = REBUILD_DISK;
+	} else if (strcmp(cmd, "rebuild-log") == 0) {
+		rebuild_type = REBUILD_LOG;
+	} else if (strcmp(cmd, "rebuild") == 0) {
+		rebuild_type = probe_dir(&store_ufs_info);
+	} else {
+		usage(argv[0]);
+		exit(1);
+	}
+
+	/* Output swap header to stdout */
+	(void) storeSwapLogPrintHeader(stdout);
+
+	if (rebuild_type == REBUILD_DISK)
+		rebuild_from_dir(&store_ufs_info);
+	else
+		rebuild_from_log(&store_ufs_info);
+
+	store_ufs_done(&store_ufs_info);
+	(void) storeSwapLogPrintCompleted(stdout);
+	fflush(stdout);
+
+	return 0;
+}
