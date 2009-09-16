4009ddc83f4493545d36f859bc7bca6b0dce3c83
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-09-16 05:10:50 +0000

    From playpen/LUSCA_HEAD_http_vector - implement some new vector operations

--- include/Vector.h
+++ include/Vector.h
@@ -8,6 +8,7 @@ struct _vector_t {
 	void *data;
 };
 typedef struct _vector_t vector_t;
+typedef struct _vector_t Vector;
 
 void vector_init(vector_t *v, int obj_size, int obj_count);
 void vector_done(vector_t *v);
@@ -15,6 +16,8 @@ void * vector_get_real(const vector_t *v, int offset);
 static inline void * vector_get(const vector_t *v, int offset) { return ((char *) v->data + (v->obj_size * offset)); }
 void * vector_append(vector_t *v);
 void * vector_insert(vector_t *v, int position);
+int vector_copy_item(vector_t *v, int dst, int src);
+void vector_shrink(vector_t *v, int new_size);
 
 #define	vector_numentries(v)	( (v)->used_count )
 #define	vector_size(v)		( (v)->alloc_count )
