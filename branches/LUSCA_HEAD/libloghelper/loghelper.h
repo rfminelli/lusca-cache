a14617968eb89eb0741c80a3c8ce086305071777
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2009-10-18 17:13:12 +0000

    Flesh out the logfile helper basics, stolen from the logging daemon code.

---
+++ branches/LUSCA_HEAD/libloghelper/loghelper.h
@@ -0,0 +1,24 @@
+#ifndef	__LIBLOGHELPER_LOGHELPER_H__
+#define	__LIBLOGHELPER_LOGHELPER_H__
+
+struct _loghelper_buffer {
+	char *buf;
+	int size;
+	int len;
+	int written_len;
+	dlink_node node;
+};
+typedef struct loghelper_buffer loghelper_buffer_t;
+
+struct _loghelper_instance {
+	int rfd, wfd;			/* read/write FDs to IPC helper */
+	char eol;			/* whether we are currently at the "end of packet" */
+	pid_t pid;			/* pid of helper */
+	int flush_pending;		/* whether a flush command is pending */
+	dlink_list bufs;		/* buffers to write */
+	int nbufs;			/* number of buffers to write */
+	int last_warned;
+};
+typedef struct _loghelper_instance loghelper_instance_t;
+
+#endif
