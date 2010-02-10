5c12b513dada32bec88fb622f78ccfc90ec033f4
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2010-02-10 16:28:07 +0000

    Begin breaking out the client-side request routines into a separate file. They'll eventually be further broken out into multiple files, one per module, with a well-defined API to tie it all together.

---
+++ branches/LUSCA_HEAD/src/client_side_request.h
@@ -0,0 +1,8 @@
+#ifndef	__CLIENT_SIDE_REQUEST_H__
+#define	__CLIENT_SIDE_REQUEST_H__
+
+/* This begins the request-side processing chain */
+extern void clientCheckFollowXForwardedFor(void *data);
+
+#endif
+
--- branches/LUSCA_HEAD/src/protos.h
+++ branches/LUSCA_HEAD/src/protos.h
@@ -1074,6 +1074,7 @@ extern void clientInterpretRequestHeaders(clientHttpRequest * http);
 extern void clientAccessCheck2(void *data);
 extern void clientFinishRewriteStuff(clientHttpRequest * http);
 extern int connStateGetCount(void);
+extern StoreEntry *clientCreateStoreEntry(clientHttpRequest *, method_t *, request_flags);
 
 /* client_side_nat.c */
 extern int clientNatLookup(ConnStateData * conn);
