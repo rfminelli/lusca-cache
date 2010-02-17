0f15ee321da77fc5ec5459863baa34e24a6fe335
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2010-02-17 04:47:04 +0000

    Migrate clientCheckRangeForceMiss() out of client_side.c

--- src/client_side_ranges.h
+++ src/client_side_ranges.h
@@ -8,5 +8,6 @@ extern void clientPackRange(clientHttpRequest * http, HttpHdrRangeIter * i, cons
 extern int clientCanPackMoreRanges(const clientHttpRequest * http, HttpHdrRangeIter * i, size_t size);
 extern int clientPackMoreRanges(clientHttpRequest * http, const char *buf, size_t size, MemBuf * mb);
 extern void clientBuildRangeHeader(clientHttpRequest * http, HttpReply * rep);
+extern int clientCheckRangeForceMiss(StoreEntry * entry, HttpHdrRange * range);
 
 #endif
