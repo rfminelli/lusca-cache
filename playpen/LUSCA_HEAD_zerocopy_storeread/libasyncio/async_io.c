17a6e82af1c8489a368330f073eec8b35b9343b5
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2010-02-20 16:57:13 +0000

    Force AIO_READ operations to always maintain a valid callback data - ie, there will always be a completion callback being made.

--- playpen/LUSCA_HEAD_zerocopy_storeread/libasyncio/async_io.c
+++ playpen/LUSCA_HEAD_zerocopy_storeread/libasyncio/async_io.c
@@ -242,6 +242,8 @@ aioCancel(int fd)
 	    ctrlp->done_handler = NULL;
 	    ctrlp->done_handler_data = NULL;
 	    debug(32, 0) ("this be aioCancel. Danger ahead!\n");
+	    if (ctrlp->operation == _AIO_READ)
+	        assert(cbdataValid(their_data));
 	    if (cbdataValid(their_data))
 		done_handler(fd, their_data, NULL, -2, -2);
 	    cbdataUnlock(their_data);
@@ -445,6 +447,8 @@ aioCheckCallbacks(void)
 	    their_data = ctrlp->done_handler_data;
 	    ctrlp->done_handler = NULL;
 	    ctrlp->done_handler_data = NULL;
+	    if (ctrlp->operation == _AIO_READ)
+	        assert(cbdataValid(their_data));
 	    if (cbdataValid(their_data)) {
 		retval = 1;	/* Return that we've actually done some work */
 		done_handler(ctrlp->fd, their_data, ctrlp->bufp,
