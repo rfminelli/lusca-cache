7bfb0a04f0f30a883fb34a71398e7dba5fb9b104
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2010-05-16 13:52:14 +0000

    Add a test case for the insertion logic. This handles the case of inserting in the middle of an already filled out array; it doesn't test the behaviour of inserting -past- the last member of the array.

--- test-suite/atf/lib_Vector.c
+++ test-suite/atf/lib_Vector.c
@@ -90,10 +90,41 @@ ATF_TC_BODY(Vector_test_3, tc)
 	vector_done(&v);
 }
 
+/* ** */
+ATF_TC(Vector_insert_1);
+ATF_TC_HEAD(Vector_insert_1, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Vector insert checks");
+}
+ATF_TC_BODY(Vector_insert_1, tc)
+{
+	Vector v;
+	int i, *n;
+
+	vector_init(&v, sizeof(int), 128);
+	for (i = 0; i < 16; i++) {
+		n = (int *) vector_append(&v);
+		ATF_REQUIRE(n != NULL);
+		(*n) = i;
+		//printf("%d: %p; used %d\n", i, n, v.used_count);
+	}
+
+	for (i = 0; i < 32; i += 2) {
+		n = (int *) vector_insert(&v, i);
+	}
+	for (i = 0; i < v.used_count; i++) {
+		n = (int *) vector_get_real(&v, i);
+		ATF_REQUIRE(n != NULL);
+		//printf("%d: %d (%d)\n", i, (*n), i / 2);
+		ATF_REQUIRE_EQ( (*n), i / 2);
+	}
+}
+
 
 ATF_TP_ADD_TCS(tp)
 {
         ATF_TP_ADD_TC(tp, Vector_test_1);
         ATF_TP_ADD_TC(tp, Vector_test_2);
         ATF_TP_ADD_TC(tp, Vector_test_3);
+        ATF_TP_ADD_TC(tp, Vector_insert_1);
 }
