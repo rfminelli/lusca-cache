0b5c1d5e2af1bb6d528b2c559768b4fac83b879d
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2010-05-23 07:40:23 +0000

    Begin breaking out the protocol_t stuff.

---
+++ libsqurl/proto.c
@@ -0,0 +1,21 @@
+#include "../include/config.h"
+
+#include <stdio.h>
+
+const char *ProtocolStr[] =
+{
+    "NONE",
+    "http",
+    "ftp",
+    "gopher",
+    "wais",
+    "cache_object",
+    "icp",
+    "htcp",
+    "urn",
+    "whois",
+    "internal",
+    "https",
+    "TOTAL"
+};
+
