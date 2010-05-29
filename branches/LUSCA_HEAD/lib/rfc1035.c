9676689ab6b81da3ed170f1271f86ec951ef04d5
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2010-05-29 05:43:29 +0000

    Merge in r13269 from /branches/ipv6_dns - import Husni's AAAA query routine from his Squid-2.6 work.
    This code is currently not linked in anywhere.

--- branches/LUSCA_HEAD/lib/rfc1035.c
+++ branches/LUSCA_HEAD/lib/rfc1035.c
@@ -661,6 +661,32 @@ rfc1035BuildAQuery(const char *hostname, char *buf, size_t sz, unsigned short qi
     return offset;
 }
 
+ssize_t
+rfc1035BuildAAAAQuery(const char *hostname, char *buf, size_t sz, unsigned short qid, rfc1035_query * query)
+{
+	static rfc1035_message h;
+	size_t offset = 0;
+	memset(&h, '\0', sizeof(h));
+	h.id = qid;
+	h.qr = 0;
+	h.rd = 1;
+	h.opcode = 0;		/* QUERY */
+	h.qdcount = 1;
+	offset += rfc1035HeaderPack(buf + offset, sz - offset, &h);
+ 	offset += rfc1035QuestionPack(buf + offset,
+	    sz - offset,
+	    hostname,
+	    RFC1035_TYPE_AAAA,
+	    RFC1035_CLASS_IN);
+	if (query) {
+		query->qtype = RFC1035_TYPE_AAAA;
+		query->qclass = RFC1035_CLASS_IN;
+		xstrncpy(query->name, hostname, sizeof(query->name));
+	}
+	assert(offset <= sz);
+	return offset;
+}
+
 /*
  * rfc1035BuildPTRQuery()
  * 
