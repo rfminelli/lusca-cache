55b040ea3eb598f7f0a86881aa2eae92e372a30c
adrian.chadd <adrian.chadd@b9cb26c4-da8a-11dd-9f21-5bfbb98aebe5>
2010-10-19 02:22:25 +0000

    Add further debugging for Issue #107

--- src/store_vary.c
+++ src/store_vary.c
@@ -586,8 +586,10 @@ storeLocateVary(StoreEntry * e, int offset, const char *vary_data, String accept
     state->seen_offset = offset;
     if (!strLen2(e->mem_obj->reply->content_type) || strCmp(e->mem_obj->reply->content_type, "x-squid-internal/vary") != 0) {
 	/* This is not our Vary marker object. Bail out. */
-	debug(33, 1) ("storeLocateVary: Not our vary marker object, %s = '%s', vary_data='%s' ; accept_encoding='%.*s'\n",
+	debug(33, 1) ("storeLocateVary: Not our vary marker object, %s = '%s', vary_data='%s' ; content-type: '%.*s' ; accept_encoding='%.*s'\n",
 	    storeKeyText(e->hash.key), e->mem_obj->url, vary_data,
+	    strLen2(e->mem_obj->reply->content_type) ? strLen2(e->mem_obj->reply->content_type) : 1,
+	    strBuf2(e->mem_obj->reply->content_type) ? strBuf2(e->mem_obj->reply->content_type) : "-",
 	    strLen2(accept_encoding) ? strLen2(accept_encoding) : 1,
 	    strBuf2(accept_encoding) ? strBuf2(accept_encoding) : "-");
 
